5d536581e912b3c9e5533f782c2c527d1bff2421
Francesco Gobbi <numeriprimi96@gmail.com>
2023-09-15 08:18:13 +0200

    add C file

--- 4IA_2023_2024/0_RipassoTeoria/0_Vettori.c
+++ 4IA_2023_2024/0_RipassoTeoria/0_Vettori.c
@@ -3,16 +3,19 @@
 #define dim 10
 
 int main() {
-    int vet[dim] = {0};
-    int i;
+    int vet[dim] = {0}; // Create a new vector
+    int i; // index of vector
 
-    for(i=0; i<dim, i++){
+    // Request number in vector
+    for(i=0; i<dim; i++){
         printf("Inserisci un valore in posizione %d: ", i);
         scanf("%d", &vet[i]);
     }
 
+    // Print all number in vector
     printf("\nIl vettore è così composto:\n");
-    for(i=0; i<dim, i++){
-        printf("%4d: ", vet[i]);
+    for(i=0; i<dim; i++){
+        printf("%4d", vet[i]);
     }
+    printf("\n"); // new line
 }
