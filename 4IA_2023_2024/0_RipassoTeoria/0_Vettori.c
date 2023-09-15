d86048df3eae652448d88091a765a449dcc96ecc
FrancescoGobbi <43830655+FrancescoGobbi@users.noreply.github.com>
2023-09-15 23:05:24 +0200

    Update 0_Vettori.c

--- 4IA_2023_2024/0_RipassoTeoria/0_Vettori.c
+++ 4IA_2023_2024/0_RipassoTeoria/0_Vettori.c
@@ -2,27 +2,33 @@
 
 #define dim 10
 
-void stampaVet(int vet[], int DIM);
+void stampaVet(int _vet[], int DIM);
+void inserisciVal(int _vet[], int DIM);
 
 int main() {
     int vet[dim] = {0}; // Create a new vector
-    int i; // index of vector
-
+    
     // Request number in vector
-    for(i=0; i<dim; i++){
-        printf("Inserisci un valore in posizione %d: ", i);
-        scanf("%d", &vet[i]);
-    }
+    inserisciVal(vet, dim);
 
     // Print all number in vector
-    stampaVet(vet,dim);
+    stampaVet(vet, dim);
 }
 
-void stampaVet(int vet[], int DIM){
+void stampaVet(int _vet[], int DIM){
     int i;
     printf("\nIl vettore è così composto:\n");
     for(i=0; i<dim; i++){
-        printf("%4d", vet[i]);
+        printf("%4d", _vet[i]);
     }
     printf("\n"); // new line
+}
+
+void inserisciVal(int _vet[], int DIM) {
+    int i; // index of vector
+
+    for(i=0; i<DIM; i++){
+        printf("Inserisci un valore in posizione %d: ", i);
+        scanf("%d", &_vet[i]);
+    }
 }
