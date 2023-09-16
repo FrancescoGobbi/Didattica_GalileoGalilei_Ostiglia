d134759d523bbb96b7814d5b69700ac0172f0597
Francesco Gobbi <numeriprimi96@gmail.com>
2023-09-16 21:42:55 +0200

    add C files

---
+++ 4IA_2023_2024/0_RipassoTeoria/4_InsertionSort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define dim 10
+
+void stampaVet(int vet[], int DIM);
+void insertioSort(int vet[], int DIM);
+void swap(int *x, int *y);
+
+int main() {
+    int vet[dim] = {3,4,5,2,1,0,6,8,9,7}; // Create a new vector
+    int i; // index of vector
+
+    printf("Vettore prima dell'ordinamento: ");
+    stampaVet(vet, dim);
+    insertioSort(vet, dim);
+    printf("Vettore dopo dell'ordinamento: ");
+    stampaVet(vet, dim);
+}
+
+void stampaVet(int vet[], int DIM){
+    int i;
+    printf("\nIl vettore è così composto:\n");
+    for(i=0; i<DIM; i++){
+        printf("%4d", vet[i]);
+    }
+    printf("\n"); // new line
+}
+
+void swap(int *x, int *y) {
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+void insertioSort(int vet[], int DIM) {
+    int i, j;
+    int key;
+
+    for(i=1; i<DIM; i++) {
+        key = vet[i];
+        j = i-1;
+
+        while(j>=0 && vet[j]>key) {
+            vet[j+1] = vet[j];
+            j--;
+        }
+        vet[j+1] = key;
+    }
+}
