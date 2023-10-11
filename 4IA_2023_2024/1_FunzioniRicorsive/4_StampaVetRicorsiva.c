3c1680d36bc8be2dd2afdd2ee5d0bd4d120ef20c
Francesco Gobbi <numeriprimi96@gmail.com>
2023-10-11 13:27:57 +0200

    add C file

---
+++ 4IA_2023_2024/1_FunzioniRicorsive/4_StampaVetRicorsiva.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define DIM 10
+
+void stampaVetRicorsiva(int vet[], int dim, int index);
+
+int main() {
+    int vet[DIM] = {1,2,3,4,5,6,7,8,9,10};
+
+    stampaVetRicorsiva(vet, DIM, 0);
+    printf("\n");
+}
+
+void stampaVetRicorsiva(int vet[], int dim, int index) {
+    if(index<dim) {
+        printf("%3d", vet[index]);
+        stampaVetRicorsiva(vet, dim,index+1);
+    }
+}
