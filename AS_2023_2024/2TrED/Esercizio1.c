d41afbb5244ad35ac26e2c2ab852382534714a46
Francesco Gobbi <numeriprimi96@gmail.com>
2023-11-03 13:31:54 +0100

    add file

---
+++ AS_2023_2024/2TrED/Esercizio1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int c;
+    int somma = 0;
+    // Riga di codice alternativa --> int a, b, c somma=0;
+
+    // Stampa per l'utente
+    printf("Inserisci 3 numeri: ");
+    // Inserimento delle 3 variabili
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+    // Riga di codice alternativa --> scanf("%d %d %d", &a, &b, &c);
+
+    // Eseguo la somma
+    somma = a + b + c;
+
+    // Stampa finale
+    printf("La somma è: %d\n", somma);
+}
