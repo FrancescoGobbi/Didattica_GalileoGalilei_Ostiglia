cf4fd3a8818b018191a244c1550ddb991752772c
Francesco Gobbi <numeriprimi96@gmail.com>
2023-11-10 12:32:41 +0100

    add file C

---
+++ AS_2023_2024/2TrED/Teoria/Teoria3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Creo le variabili
+    int A, B; 
+
+    // Prendo in input i valori
+    scanf("%d %d", &A, &B);
+
+    // Selezione
+    if( A > B ) { // Caso VERO  
+        printf("Il valore maggiore è: %d\n", A);
+        // N.B. lo \n permette di andare a capo in una stampa
+        // Quindi oltre alla stampa del messaggio, poi si andrà a capo
+    }
+    else { // Caso FALSO
+        printf("Il valore maggiore è: %d\n", B);
+    }
+}
