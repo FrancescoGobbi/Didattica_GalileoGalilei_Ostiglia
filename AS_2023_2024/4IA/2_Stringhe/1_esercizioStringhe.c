e5e6245805762077d220ecccc11fd54f9a9728af
Francesco Gobbi <numeriprimi96@gmail.com>
2023-11-14 11:30:28 +0100

    add file C

---
+++ AS_2023_2024/4IA/2_Stringhe/1_esercizioStringhe.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+const int dim = 20;
+
+/**
+ * @brief funzione che pranda una stringa in input e 
+ * la modifica strasformando tutte le lettere minuscole
+ * in minuscolo
+ * 
+ * ESEMPIO: str "ciao"
+ * --> poi str sarà "CIAO"
+ * 
+ */
+// TO DO
+
+/**
+ * @brief funzione che pranda due stringhe in input 
+ * una str e una dst. La funzione deve mettere in coda
+ * alla stringa dst anche la stringa str.
+ * 
+ * ESEMPIO: str = "come" e dst = "ciao"
+ * --> poi dst sarà "ciaocome"
+ * 
+ */
+// TO DO
+
+int main() {
+    char str[dim];
+}
