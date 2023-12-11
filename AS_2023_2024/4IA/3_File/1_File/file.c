37f22f7246f96e295d19953d0a629c8004585cc6
FrancescoGobbi <francesco.gobbi4@gmail.com>
2023-12-11 15:43:09 +0100

    add file C

---
+++ AS_2023_2024/4IA/3_File/1_File/file.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // Nome del file
+    char nome_file[] = "prova.txt";
+
+    // Messaggio da scrivere nel file
+    char messaggio[] = "Questo è un messaggio semplice.";
+
+    // Apre il file in modalità scrittura ("w" sta per write)
+    FILE *file = fopen(nome_file, "w"); // Puntatore al file
+
+    // Verifica se il file è stato aperto correttamente
+    if (file == NULL) {
+        printf("Errore nell'apertura del file.\n");
+        return 1; // Indica un errore
+    }
+
+    // Scrive il messaggio nel file
+    fprintf(file, "%s\n", messaggio);
+
+    // Chiude il file
+    fclose(file);
+
+    printf("Il messaggio è stato scritto con successo nel file.\n");
+
+    return 0; // Indica che il programma è stato eseguito con successo
+}
