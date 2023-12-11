1fe1e3c218a67ba2611963e5203400addd96ac9b
FrancescoGobbi <francesco.gobbi4@gmail.com>
2023-12-11 16:42:18 +0100

    add file C

---
+++ AS_2023_2024/4IA/3_File/5_InputTastieraOutputFile/outputFile.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    // Nome del file in cui scrivere i nomi
+    char nome_file[] = "prova.txt";
+
+    // Apre il file in modalità scrittura ("w" sta per write)
+    FILE *file = fopen(nome_file, "w"); // Puntatore del file, sempre in testa al file
+
+    // Verifica se il file è stato aperto correttamente
+    if (file == NULL) {
+        printf("Errore nell'apertura del file.\n");
+        return 1; // Indica un errore
+    }
+
+    // Buffer per leggere i nomi
+    char nome[100];
+
+    // Chiede all'utente di inserire i nomi e li scrive sul file
+    printf("Inserisci %d nomi:\n", num_nomi);
+    for (int i = 1; i <= 5; ++i) {
+        printf("Nome %d: ", i);
+        scanf("%s", nome);
+        fprintf(file, "Nome %d: %s\n", i, nome);
+    }
+
+    // Chiude il file
+    fclose(file);
+
+    printf("I nomi sono stati scritti con successo nel file %s.\n", nome_file);
+}
