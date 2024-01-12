f2c94c9b2f5c348d581614e8bc899c2778db5687
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-01-12 12:36:54 +0100

    add file

--- AS_2023_2024/2TrED/Esercizi/Esercizio_Prova_Consegna.c
+++ AS_2023_2024/2TrED/Esercizi/Esercizio_Prova_Consegna.c
@@ -5,10 +5,13 @@ Creare un programma che prende in input due valori, li somma in una nuova variab
 int main() {
     int a, b, somma;
     
+    // Input dei valori da assegnare alle variabili
     printf("Inserisci due valori interi: ");
     scanf("%d %d", &a, &b);
     
+    // Creo la somma nella corrispondente variabile
     somma = a + b;
 
+    // Stampa del risultato
     printf("%d + %d = %d\n", a, b, somma);
 }
