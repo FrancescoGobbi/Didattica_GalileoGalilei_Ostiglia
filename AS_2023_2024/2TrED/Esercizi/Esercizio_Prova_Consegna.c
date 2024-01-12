d13613de199f8e4efa07f2d0380d37a0980724e2
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-01-12 12:36:09 +0100

    add file

--- AS_2023_2024/2TrED/Esercizi/Esercizio_Prova_Consegna.c
+++ AS_2023_2024/2TrED/Esercizi/Esercizio_Prova_Consegna.c
@@ -3,5 +3,12 @@
 Creare un programma che prende in input due valori, li somma in una nuova variabile e stampa il risultato a video
 */
 int main() {
+    int a, b, somma;
     
+    printf("Inserisci due valori interi: ");
+    scanf("%d %d", &a, &b);
+    
+    somma = a + b;
+
+    printf("%d + %d = %d\n", a, b, somma);
 }
