20c17dac3873643597c94e2ac9960c971a332615
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-01-23 22:34:08 +0100

    add file

---
+++ AS_2023_2024/4IA/3_File/Esercizi/Esercizio0.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define DIM 20
+
+int main() {
+    char file_name[] = "prova.txt";
+    char str[DIM];
+    int i;
+
+    FILE * fpIn = fopen(file_name, "rt");
+
+    while(!feof(fpIn)) {
+        fscanf(fpIn, "%s", str);
+        printf("%s ovvero %d\n", str, str[0]);
+        
+        for(i = 0; i < DIM; i++) {
+            printf("%5d", str[i]);
+        }
+        printf("\n");
+        
+        printf("Entro nel while\n");
+    }
+
+    fclose(fpIn);
+
+    // Riapro il file per la lettura
+    fpIn = fopen(file_name, "rt");
+
+    while (fgets(str, sizeof(str), fpIn) != NULL) {
+        printf("%s\n", str);
+        printf("Entro nel while\n");
+    }
+    fclose(fpIn);
+}
