8e2fd6f20246b77a71b22af4a4c56fcfa8b67d5d
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-02-01 15:11:09 +0100

    add file

--- AS_2023_2024/2TrED/Teoria/Teoria9_For.c
+++ AS_2023_2024/2TrED/Teoria/Teoria9_For.c
@@ -6,7 +6,7 @@
 int main() {
     // Salvo in n il valore che ogni volta l'utente mi inserirà in input
     // Poi eseguirò il prodotto nella variaible prodotto
-    int n, prodotto = 1, i = 1;
+    int n, prodotto = 1, i;
 
     // Iterazione per chiedere in input i 3 valori
     //for (inizializzazione della variabile contatore; controlllo della variabile contatore; incremento/decremento della variabile contatore){
@@ -17,7 +17,6 @@ int main() {
         scanf("%d", &n); // Salvo in n il valore che ogni volta l'utente mi inserirà in input
 
         prodotto = prodotto * n; // prodotto *= n;
-        i++; // i = i + 1;
     }
 
     printf("Il prodotto dei valori inseriti è: %d\n", prodotto);
