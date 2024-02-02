d8dc31b17199b30f59b7541acd783057193d1405
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-02-02 12:46:40 +0100

    add file

--- AS_2023_2024/2TrED/Teoria/Teoria10.c
+++ AS_2023_2024/2TrED/Teoria/Teoria10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-// Chiedere in input 3 valori interi e farne il prodotto
-// ESEMPIO: i tre valori interiti sono -> -3, 4, 5 . Quindi il prodotto sarà: -3 * 4 * 5 = -60
+// Chiedere in input 3 valori interni e strettamente maggiori di 0 e farne il prodotto
+// ESEMPIO: i tre valori interiti sono -> 3, 4, 5 . Quindi il prodotto sarà: 3 * 4 * 5 = 60
 // ARGOMENTO: while (iterazioni)
 
 int main() {
@@ -11,8 +11,14 @@ int main() {
 
     // Iterazione per chiedere in input i 3 valori
     while(i <= 3) {
+
+        // Iterazione do-while: iterazione con controllo in coda
+        // Per prendere in input un valore intero e continuare
+        // a richiederelo in input finché il valore interito è minore o uguale a 0
+        do{
         printf("Inserisci un valore: ");
         scanf("%d", &n); // Salvo in n il valore che ogni volta l'utente mi inserirà in input
+        }while(n<=0); // Condizione per far continuare il loop
 
         prodotto = prodotto * n; // prodotto *= n;
         i++; // i = i + 1;
