fe3317f89392f83038cf8b663fc461bcce1ae685
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-02-02 12:51:20 +0100

    add file C

--- AS_2023_2024/2TrED/Teoria/Teoria10_For.c
+++ AS_2023_2024/2TrED/Teoria/Teoria10_For.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-// Chiedere in input 3 valori e farne il prodotto
+// N.B. Esercizio 9, con una modifica sostanziale!
+// Chiedere in input 3 valori interni e strettamente maggiori di 0 e farne il prodotto
+// ESEMPIO: i tre valori interiti sono -> 3, 4, 5 . Quindi il prodotto sarà: 3 * 4 * 5 = 60
 // ARGOMENTO: For (Iterazioni)
 
 int main() {
@@ -13,10 +15,17 @@ int main() {
     //    ...istruzioni nel cilo for
     //}
     for(i = 1; i <= 3; i++) {
-        printf("Inserisci un valore: ");
+
+        // Iterazione do-while: iterazione con controllo in coda
+        // Per prendere in input un valore intero e continuare
+        // a richiederelo in input finché il valore interito è minore o uguale a 0
+        do{
+        printf("Inserisci un valore strettamente maggiore di 0: ");
         scanf("%d", &n); // Salvo in n il valore che ogni volta l'utente mi inserirà in input
+        }while(n<=0); // Condizione per far continuare il loop
 
         prodotto = prodotto * n; // prodotto *= n;
+        i++; // i = i + 1;
     }
 
     printf("Il prodotto dei valori inseriti è: %d\n", prodotto);
