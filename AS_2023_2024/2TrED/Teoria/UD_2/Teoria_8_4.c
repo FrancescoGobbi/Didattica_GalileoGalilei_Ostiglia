e7e968857cd068cdb76fe029de501c374784799c
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-02-08 11:57:43 +0100

    add file

---
+++ AS_2023_2024/2TrED/Teoria/UD_2/Teoria_8_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+Creare un programma in C che chieda in input un valore n (n>0)
+e stampi un triangolo al contrario formato da asterischi in base alla dimensione della variabile n
+
+ESEMPIO: se n = 4
+La stampa dovrà essere:
+* * * *
+  * * *
+    * *
+      *
+      
+*/
