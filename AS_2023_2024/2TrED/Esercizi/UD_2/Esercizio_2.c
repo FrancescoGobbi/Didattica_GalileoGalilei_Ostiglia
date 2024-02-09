d265f6fb2edc600e589a9db97452b0c7e8a3b8ec
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-02-10 00:24:20 +0100

    add file C Esercizi

---
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+Creare un codice in C stampi 5 volte il messaggio "Ciao, come stai?"
+Il codice deve andare a capo ogni volta che viene stampato il messaggio sopra.
+Il codice deve andare a capo ogni volta che viene stampato il messaggio sopra.
+*/
+
+int main() {
+
+}
---
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+Creare un codice in C che prenda in input 5 valori interi.
+Ad ogni inserimento del valore in input il programma deve verificare
+se il valore in quel momento è >=10.
+Se SI, il programma stamperà "OK!", altrimenti "NO!"
+*/
+
+int main() {
+
+}
