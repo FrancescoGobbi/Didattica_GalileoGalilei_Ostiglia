cc7ebd8f30ffd91b4c3474f62b44bb4c6e4b1760
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-02-14 14:29:55 +0100

    add file C

---
+++ AS_2023_2024/4IA/4_Strutture/Strutture0.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+// Definizione di una struttura 'Persona'. 'Persona' è un nuovo tipo di struttura definita nel programma
+struct Persona {
+    char nome[50];
+    int eta;
+    float altezza;
+};
+
+int main() {
+    // Dichiarazione di una variabile di tipo 'Persona'
+    // persona1 e persona2 sono due variabili struttura di tipo 'Persona'
+    struct Persona persona1;
+    struct Persona persona2;
+
+    // Assegnazione di valori ai membri della struttura
+    // Funzione strcpy: funzione della libreria <string.h>
+    // Funzione che copia la stringa, come secondo parametro, all'interno del primo parametro
+    strcpy(persona1.nome, "Mario"); 
+    persona1.eta = 30;
+    persona1.altezza = 1.75;
+
+    // Stampa dei valori dei membri della struttura
+    printf("Nome: %s\n", persona1.nome);
+    printf("Età: %d\n", persona1.eta);
+    printf("Altezza: %.2f\n", persona1.altezza);
+
+    printf("\n\n\n");
+
+    // Prendo dei parametri da input da inserire nella struttura
+    printf("Inserisci un nome della persona: ");
+    scanf("%s", persona2.nome); // N.B. persona2.nome è quindi un puntatore al vettore di char della struttura
+    
+    printf("Inserisci l'età della persona: ");
+    scanf("%d", &persona2.eta);
+
+    printf("Inserisci l'altezza della persona: ");
+    scanf("%f", &persona2.altezza);
+
+    // Stampa dei valori dei membri della struttura
+    printf("Nome: %s\n", persona2.nome);
+    printf("Età: %d\n", persona2.eta);
+    printf("Altezza: %.2f\n", persona2.altezza);
+}
---
+++ AS_2023_2024/4IA/4_Strutture/readme.md
@@ -0,0 +1,4 @@
+### *File e funzioni con l'uso delle strutture*
+
+**File .pdf**  
+Parte teorica sui file e sulle funzioni con l'uso dei file.  
