b709e4dd15b820fa9a3f52d3bac5208ed36f1200
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-02-14 22:47:17 +0100

    add file C

--- AS_2023_2024/4IA/4_Strutture/Strutture1.c
+++ AS_2023_2024/4IA/4_Strutture/Strutture1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 // Struttura Punto --> punto del Piano Cartesiano
 // La struttura ha due parametri
@@ -7,13 +8,62 @@ struct Punto {
     int y; // Ordinata
 };
 
+/**
+ * @brief Funzione che prende due punti del Piano Cartesiano e restituisce la distanza tra i due punti
+ * @param int Ascissa punto 1
+ * @param int Ordinata punto 1
+ * @param int Ascissa punto 2
+ * @param int Ordinata punto 2
+ * 
+ * @return float 
+ */
+float distanzaDuePunti(int, int, int, int);
+
+/**
+ * @brief Funzione che prende due punti del Piano Cartesiano, utilizzando i puntatori
+ * e restituisce la distanza tra i due punti
+ * @param Struct Punto --> Puntatore al punto 1
+ * @param Struct Punto --> Puntatore al punto 2
+ * 
+ * @return float 
+ */
+float distanzaDuePuntiPuntatori(struct Punto *, struct Punto *);
+
 int main() {
     // Dichiarazione di una variabile di tipo 'Punto' (struttura di tipo 'Punto')
     struct Punto punto1;
+    struct Punto punto2;
 
-    // Assegnazione dei valori ai parametri della struttura
-    punto1.x = 10;
-    punto1.y = 20;
+    printf("Inserisci il primo punto:\n");
+    printf("Inserisci l'ascissa: ");
+    scanf("%d", &(punto1.x));
+    printf("Inserisci l'ordianta: ");
+    scanf("%d", &(punto1.y));
 
-    printf("Coordinate del punto: (%d, %d)\n", punto1.x, punto1.y);
+    printf("\nInserisci il secondo punto:\n");
+    printf("Inserisci l'ascissa: ");
+    scanf("%d", &(punto2.x));
+    printf("Inserisci l'ordianta: ");
+    scanf("%d", &(punto2.y));
+
+    printf("Calcolo la distanza tra i due punti con passaggio di valori...\n"); 
+    printf("La distanza tra i due punti è: %f\n", distanzaDuePunti(punto1.x, punto1.y, punto2.x, punto2.y));
+
+    printf("Calcolo la distanza tra i due punti con passaggio di riferimento...\n"); 
+    printf("La distanza tra i due punti è: %f\n", distanzaDuePuntiPuntatori(&punto1, &punto2));
+}
+
+float distanzaDuePunti(int x1, int y1, int x2, int y2) {
+    // Calcolo distanza tra due punti
+    return pow(pow((x1-x2),2) + pow((y1-y2), 2), 0.5);
+}
+
+float distanzaDuePuntiPuntatori(struct Punto *p1, struct Punto *p2) {
+    // Calcolo distanza tra due punti
+    /*
+    N.B. struct Punto *p1 è un puntatore alla prima struttura che abbiamo passato per riferimento
+    Per accedere ai campi delle struttura passate come riferimento si deve fare:
+    "nomeDelPuntatoreDellaStruttura"->"campoDaAccedereDellaStruttura"
+    */
+    return pow(pow((p1->x - p2->x),2) + pow((p1->y - p2->y), 2), 0.5);
 }
