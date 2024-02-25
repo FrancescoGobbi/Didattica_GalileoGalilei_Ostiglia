fcd870b60549ed0b10cd86d826b0cf829feb1881
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-02-25 21:49:53 +0100

    add Esercizi

---
+++ AS_2023_2024/4IA/4_Strutture/Esercizi/Esercizio1.c
@@ -0,0 +1,25 @@
+/*
+TESTO: 
+Creare una struttura che possa ricreare le informazioni
+di un video-gioco con nome, casa edistrice, tipo di gioco.
+Il programma deve creare due giochi, chiederli in input
+correttamente. Successivamente deve stampare i dati di ciascuna struttura.
+
+N.B. Sia l'input dei dati e sia l'output devono essere
+gestiti attraverso delle funzioni.
+Quindi funzioni passando una struttura.
+
+*/
+
+// Importare le librerie
+
+// Definizione della struttura
+
+// Definizione delle funzioni
+
+// main()
+int main() {
+
+}
+
+// Funzioni utilizzare
