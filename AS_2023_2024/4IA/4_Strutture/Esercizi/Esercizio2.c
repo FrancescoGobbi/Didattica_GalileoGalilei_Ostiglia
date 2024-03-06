1479cba102f7c5ca1d7ccaeb5d63c6780b32538c
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-03-06 11:45:38 +0100

    add file C

--- AS_2023_2024/4IA/4_Strutture/Esercizi/Esercizio2.c
+++ AS_2023_2024/4IA/4_Strutture/Esercizi/Esercizio2.c
@@ -14,6 +14,7 @@ tutti i voti e la media del singolo studente. Se lo studente non è presente seg
 // Importare le librerie
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Definizione della struttura
 typedef struct {
@@ -25,7 +26,138 @@ typedef struct {
     int informatica;
 } Studente;
 
+// Funzione 1
+void inputStudente(char []);
 
+// Funzione 2
+void stampaStudenti(char []);
+
+// Funzione 3
+void mediaStudenti(char []);
+
+// Funzione 4
+void mediaSingoloStudente(char []);
+
+// Funzione 5
 int main() {
+    inputStudente("file.dat");
 
+    stampaStudenti("file.dat");
+
+    mediaStudenti("file.dat");
+
+    mediaSingoloStudente("file.dat");
 }
+
+void inputStudente(char fileName[]) {
+    // Apro il file per l'append binario
+    FILE *fOut = fopen(fileName, "ab");
+    
+    // Creo un puntatore ad una struttura ed alloco la memoria
+    Studente * s = malloc(sizeof(Studente));
+
+    printf("Inserisci il nome dello studente: ");
+    scanf("%s", s->nome);
+    fflush(stdin);
+    printf("Inserisci il cognome dello studente: ");
+    scanf("%s", s->cognome);
+    fflush(stdin);
+    printf("Inserisci il voto di MATEMATICA: ");
+    scanf("%s", s->matematica);
+    fflush(stdin);
+    printf("Inserisci il voto di STORIA: ");
+    scanf("%s", s->storia);
+    fflush(stdin);
+    printf("Inserisci il voto di ITALIANO: ");
+    scanf("%s", s->italiano);
+    fflush(stdin);
+    printf("Inserisci il voto di INFORMATICA: ");
+    scanf("%s", s->informatica);
+    fflush(stdin);
+
+    // Scrivo nel file
+    fwrite(s, sizeof(Studente), 1, fOut);
+
+    // Chiude il file
+    fclose(fOut);
+}
+
+void stampaStudenti(char fileName[]) {
+    // Apro il file per l'append binario
+    FILE *fOut = fopen(fileName, "rb");
+    
+    // Creo un puntatore ad una struttura ed alloco la memoria
+    Studente * s = malloc(sizeof(Studente));
+
+    // Volendo il >=1 non serve
+    // Leggo tutto il file, struttura Studente per struttura Studente
+    // Quindi un quantitativo di byte pari alla struttura Studente
+    while(fread(s, sizeof(Studente), 1, fOut) >= 1) {
+        printf("Nome: %s\n", s->nome);
+        printf("Cognome: %s\n", s->cognome);
+        printf("Matematica: %d\n", s->matematica);
+        printf("Storia: %d\n", s->storia);
+        printf("Italiano: %d\n", s->italiano);
+        printf("Informatica: %d\n", s->informatica);
+    }
+    
+    // Chiudo il file
+    fclose(fOut);
+}
+
+void mediaStudenti(char fileName[]) {
+    // Apro il file per l'append binario
+    FILE *fOut = fopen(fileName, "rb");
+    float media;
+    
+    // Creo un puntatore ad una struttura ed alloco la memoria
+    Studente * s = malloc(sizeof(Studente));
+
+    // Volendo il >=1 non serve
+    // Leggo tutto il file, struttura Studente per struttura Studente
+    // Quindi un quantitativo di byte pari alla struttura Studente
+    while(fread(s, sizeof(Studente), 1, fOut) >= 1) {
+        printf("Nome: %s\n", s->nome);
+        printf("Cognome: %s\n", s->cognome);
+        media = (float)(s->matematica + s->italiano + s->italiano + s->storia) / 4;
+        printf("Media dello studente: %f\n", media);
+    }
+    
+    // Chiudo il file
+    fclose(fOut);
+}
+
+void mediaSingoloStudente(char fileName[]) {
+    // Apro il file per l'append binario
+    FILE *fOut = fopen(fileName, "rb");
+    float media;
+    char nomeStudente[30];
+    int check = 0;
+    
+    // Creo un puntatore ad una struttura ed alloco la memoria
+    Studente * s = malloc(sizeof(Studente));
+
+    printf("Inserisci il nome di una persona da trovare: ");
+    scanf("%s", nomeStudente);
+    fflush(stdin);
+
+    // Volendo il >=1 non serve
+    // Leggo tutto il file, struttura Studente per struttura Studente
+    // Quindi un quantitativo di byte pari alla struttura Studente
+    while(fread(s, sizeof(Studente), 1, fOut) >= 1) {
+        if(strcmp(nomeStudente, s->nome)) {
+            printf("Nome: %s\n", s->nome);
+            printf("Cognome: %s\n", s->cognome);
+            media = (float)(s->matematica + s->italiano + s->italiano + s->storia) / 4;
+            printf("Media dello studente: %f\n", media);
+            check = 1;
+        }
+    }
+    
+    // Chiudo il file
+    fclose(fOut);
+
+    if(check==0) {
+        printf("Non c'è lo studente che ha inserito!\n");
+    }
+}   
