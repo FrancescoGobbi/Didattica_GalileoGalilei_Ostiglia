3b707acefb036501838d2baff9e6c6572f41545a
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-03-15 11:01:48 +0100

    add file C

--- AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_07.c
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_07.c
@@ -12,5 +12,18 @@ questi 5 valori: 6, 5, 4, 8, 10 --> la somma sarà: 33
 */
 
 int main() {
+    int n, val, somma = 0, i;
 
+    do{
+        printf("Inserisci un valore da fare il fattoriale: ");
+        scanf("%d", &n); 
+    }while(n<=0);
+
+    for(i = 1; i <= n; i++) {
+        printf("Inserisci il (%d)° valore: ", i);
+        scanf("%d", &val);
+        somma = somma + val;
+    }
+
+    printf("La somma dei valori inseriti è: %d\n", somma);
 }
--- AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_08.c
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_08.c
@@ -12,5 +12,40 @@ questi 5 valori: 6, 5, 4, 8, 10 --> la media tra questi sarà: 6,6
 */
 
 int main() {
+    int n, val, somma = 0, i;
+    float media;
 
+    do{
+        printf("Inserisci un valore da fare il fattoriale: ");
+        scanf("%d", &n); 
+    }while(n<=0);
+
+    for(i = 1; i <= n; i++) {
+        printf("Inserisci il (%d)° valore: ", i);
+        scanf("%d", &val);
+        somma = somma + val;
+    }
+
+    media = (float)somma / n;
+    printf("La media dei valori inseriti è: %f\n", media);
 }
+
+// ALTERNATIVA
+/*
+    int n, val, i;
+    float media = 0;
+
+    do{
+        printf("Inserisci un valore da fare il fattoriale: ");
+        scanf("%d", &n); 
+    }while(n<=0);
+
+    for(i = 1; i <= n; i++) {
+        printf("Inserisci il (%d)° valore: ", i);
+        scanf("%d", &val);
+        media = media + val;
+    }
+
+    media = media / n;
+    printf("La media dei valori inseriti è: %f\n", media);
+*/
