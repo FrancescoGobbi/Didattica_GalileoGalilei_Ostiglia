35f1af2e1d706d494aa1a17fc9ef68d08173aad7
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-03-22 10:37:39 +0100

    add file C

--- AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_09.c
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_09.c
@@ -12,4 +12,23 @@ ESEMPIO: Se A = 5 e B = 3, il programma dovrà fare quindi 5 + 5 + 5 = 15
 
 int main() {
 
+    int A, B;
+    int prodotto = 0;
+    int i; // Variabile per contrare i cicli
+
+    do{
+        printf("Inserisci il valore di A: ");
+        scanf("%d", &A); 
+    }while(A<=0);
+
+    do{
+        printf("Inserisci il valore di B: ");
+        scanf("%d", &B); 
+    }while(B<=0);
+
+    for(i=1; i<=B; i++) {
+        prodotto = prodotto + A;
+    }
+
+    printf("%d * %d = %d\n", A, B, prodotto);
 }
