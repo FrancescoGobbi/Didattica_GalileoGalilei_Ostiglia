1d86dc7aa78e3ed825473fede2e998577a8195d1
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-04-03 22:29:53 +0200

    add file C

---
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// ESERCIZIO 13
+/*
+    Prendere in input due valori numerici A e B (interi e maggiori di 0).
+    N.B. A deve essere maggiore di B (A>B).
+    Il programma deve stampare tutti i numeri compresi tra A e B che siano dipari (estremi compresi!).
+
+    ESEMPIO:
+    Se A = 5 e B = 15 --> Stampo: 5, 7, 9, 11, 13, 15.
+*/
+
+int main() {
+
+}
---
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// ESERCIZIO 13
+/*
+    Prendere in input un valore A (intero e maggiore di 0).
+    Il programma deve varificare e stampare se il numero è primo o no.
+
+    N.B. Come si definisce se un numero è primo?
+
+    ESEMPIO:
+    Se A = 5 --> Il programma stamperà "5 è un numero primo!"
+    Se A = 12 --> Il programma stamperà "12 NON è un numero primo!"
+*/
+
+int main() {
+
+}
