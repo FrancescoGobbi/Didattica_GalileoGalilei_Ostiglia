046d4084ed81d30e7a63da47a03310663412d99b
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-04-10 22:30:20 +0200

    add file C

--- AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_13.c
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_13.c
@@ -3,7 +3,7 @@
 // ESERCIZIO 13
 /*
     Prendere in input due valori numerici A e B (interi e maggiori di 0).
-    N.B. A deve essere maggiore di B (A>B).
+    N.B. A deve essere maggiore di B (A<B).
     Il programma deve stampare tutti i numeri compresi tra A e B che siano dispari (estremi compresi!).
 
     ESEMPIO:
@@ -11,5 +11,24 @@
 */
 
 int main() {
+    int A, B;
+    int i;
 
+    do{
+        printf("Inserisci il valore di A: ");
+        scanf("%d", &A);
+    }while(A<=0);
+
+    do{
+        printf("Inserisci il valore di B: ");
+        scanf("%d", &B);
+    }while(B>=A);
+
+    printf("I numeri sono: ");
+    for(i = B; i<=A; i++) {
+        if(i%2 == 1) { 
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
 }
