ff0a4375e4f2e022380356877fea1afd949e5337
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-04-11 21:35:33 +0200

    add file C

--- AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_13.c
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_13.c
@@ -4,10 +4,16 @@
 /*
     Prendere in input due valori numerici A e B (interi e maggiori di 0).
     N.B. A deve essere maggiore di B (A>B).
-    Il programma deve stampare tutti i numeri compresi tra A e B che siano dispari (estremi compresi!).
+    Il programma deve stampare tutti i numeri compresi tra A e B 
+    che siano dispari (estremi compresi!).
 
     ESEMPIO:
-    Se A = 5 e B = 15 --> Stampo: 5, 7, 9, 11, 13, 15.
+    Se B = 5 e A = 15 --> Stampo: 5, 7, 9, 11, 13, 15.
+
+    A > 0 AND B > 0 AND A > B
+
+    Controllo quindi: B > 0 AND A > B
+
 */
 
 int main() {
@@ -16,14 +22,14 @@ int main() {
 
     // Input di B > 0
     do{
-        printf("Inserisci il valore di A: ");
-        scanf("%d", &A);
+        printf("Inserisci il valore di B: ");
+        scanf("%d", &B);
     }while(B<=0);
 
     // Input di A > B (quindi per forza anche A sarà > 0)
     do{
-        printf("Inserisci il valore di B: ");
-        scanf("%d", &B);
+        printf("Inserisci il valore di A: ");
+        scanf("%d", &A);
     }while(B>=A);
 
     printf("I numeri sono: ");
--- AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_14.c
+++ AS_2023_2024/2TrED/Esercizi/UD_2/Esercizio_14.c
@@ -13,5 +13,28 @@
 */
 
 int main() {
+    int A;
+    int i;
+    int contaDivisori = 0;
 
+    // Input del valore
+    do{
+        printf("Inserisci il valore di A: ");
+        scanf("%d", &A);
+    }while(A<=0);
+
+    // Centro e conto tutti i divisori di A
+    for(i = 1; i<=A; i++) {
+        if(A%i==0) { // Se i è un divisore del numero
+            contaDivisori++; // Incrmento il contatore dei divisori
+        }
+    }
+
+    // Verifico se A è un divisore
+    if (contaDivisori == 2) {
+        printf("Il valore %d è un NUMERO PRIMO!\n", A);
+    }
+    else {
+        printf("Il valore %d NON è un NUMERO PRIMO!\n", A);
+    }
 }
