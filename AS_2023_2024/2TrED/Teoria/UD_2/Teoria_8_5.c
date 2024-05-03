0c5f3c29c4276dcc8aa86db5b189285245fa6cbb
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-05-03 10:28:00 +0200

    Add file C

---
+++ AS_2023_2024/2TrED/Teoria/UD_2/Teoria_8_5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+Creare un programma in C che chieda in input un valore n (n>0)
+e stampi un Triangolo di Tartaglia.
+
+ESEMPIO: se n = 4
+La stampa dovrà essere:
+1
+2 3
+4 5 6
+7 8 9 10
+
+*/
+
+int main() {
+
+    // TO-DO..
+}
---
+++ AS_2023_2024/2TrED/Teoria/UD_2/Teoria_8_6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+Creare un programma in C che chieda in input un valore n (n>0) e m (m>0)
+e stampi un rettandolo di dimensione nxm come mostrato in figura.
+
+ESEMPIO: se n = 4 e m = 5
+La stampa dovrà essere:
+* * * * *
+*       *
+*       *
+* * * * *
+
+*/
+
+int main() {
+
+    // TO-DO..
+}
