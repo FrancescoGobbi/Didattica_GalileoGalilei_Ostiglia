5507390671190809056f7afeb938a142721b9f08
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-05-08 22:25:14 +0200

    Add file C

---
+++ AS_2023_2024/2TrED/Teoria/UD_2/Teoria_8_7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+Creare un programma in C che esegua l'intera tabella 
+delle moltiplicazioni.
+
+Vedi la seguente tabella:
+https://digilander.libero.it/sergiobor/La%20tabella%20della%20moltiplicazione.pdf
+
+*/
+
+int main() {
+
+    // TO-DO..
+}
