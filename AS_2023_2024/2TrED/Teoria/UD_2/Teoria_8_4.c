cf1f5185ee619244025a0ce54763dc976a7ccaab
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-05-09 16:39:27 +0200

    Add file C

--- AS_2023_2024/2TrED/Teoria/UD_2/Teoria_8_4.c
+++ AS_2023_2024/2TrED/Teoria/UD_2/Teoria_8_4.c
@@ -16,6 +16,46 @@ Qual è la relazione tra il numero della riga rispetto al numero di spazi e di a
 */
 
 int main() {
+  int n;
+  int i; // Per le righe della figura
+  int j; // Per le colonne della figura
+  int spazi = 0;
 
-    // TO-DO..
+  do{
+    printf("Inserisci un numero n: ");
+    scanf("%d", &n);
+  }while(n<=0);
+
+  printf("Stampo la figura:\n");
+
+  // Doppia iterazione annidata
+  for(i=1; i<=n; i++) { // For per le righe
+    // Iterazione per gli spazi
+    for (j=1; j<=spazi; j++) {
+      printf("   ");
+    }
+    // Iterazione per gli asterischi
+    for (j=1; j<=(n-i+1); j++) {
+      printf(" * ");
+    }
+    printf("\n"); // Vado a capo
+    spazi++;
+  }
+
+  printf("Stampo la figura (soluzione Daria):\n");
+  spazi = 0;
+  // Doppia iterazione annidata
+  for(i=n; i>=1; i--) { // For per le righe
+    // Iterazione per gli spazi
+    for (j=1; j<=spazi; j++) {
+      printf("   ");
+    }
+    // Iterazione per gli asterischi
+    for (j=1; j<=i; j++) {
+      printf(" * ");
+    }
+    printf("\n"); // Vado a capo
+    spazi++;
+  }
+  
 }
