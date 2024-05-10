23a77c9b1f623f9369cfcfdd81b9c106c98b7a6e
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-05-10 11:27:15 +0200

    Add file C

--- AS_2023_2024/2TrED/Teoria/UD_2/Teoria_8_6.c
+++ AS_2023_2024/2TrED/Teoria/UD_2/Teoria_8_6.c
@@ -14,6 +14,30 @@ La stampa dovrà essere:
 */
 
 int main() {
+    int n, m;
+    int i; // Per le righe
+    int j; // Per le colonne
+    int val = 1;
 
-    // TO-DO..
+    do{
+        printf("Inserisci un numero n per le righe: ");
+        scanf("%d", &n);
+    }while(n<=0);
+
+    do{
+        printf("Inserisci un numero n per le colonne: ");
+        scanf("%d", &m);
+    }while(m<=0);
+
+    for(i=1; i<=n; i++){ // Iterazione per le righe
+        for(j=1; j<=m; j++) { // Iterazione per le colonne
+            if (i==1 || i==n || j==1 || j==m) {
+                printf(" * ");
+            }
+            else {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
 }
