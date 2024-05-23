3f046ea7edda16df5fc1855be6f1634f1e01e624
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-05-23 12:11:46 +0200

    Add file C

---
+++ AS_2023_2024/2TrED/Teoria/UD_2/Teoria_8_10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+Creare un programma in C che prenda in input un 
+valore n (n>0) e che stampi il seguente disegno:
+
+Se n = 5:
+$ 0 $ 0 $
+0 $ 0 $ 0
+$ 0 $ 0 $
+0 $ 0 $ 0
+$ 0 $ 0 $
+
+N.B. Notate che il disegno è quadrato.
+*/
+
+int main() {
+    
+}
