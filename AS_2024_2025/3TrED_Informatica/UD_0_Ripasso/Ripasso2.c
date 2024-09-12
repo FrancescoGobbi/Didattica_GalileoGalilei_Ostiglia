251a108e1e8e64fb64715978c57ab5b7abf66994
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-09-12 23:45:23 +0200

    Add fil C

---
+++ AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso2.c
@@ -0,0 +1,19 @@
+/**
+ * @file Ripasso2.c
+ * @author your name 
+ * @brief Creare un programma in C che stampi questa immagine:
+ * T T T T T
+ * V V V V V
+ * T T T T T
+ * V V V V V
+ * T T T T T
+ * 
+ * N.B Controllare bene la dimensione dell'immagine
+ * @version 0.1
+ * @date 2024-09-13
+ *
+ * 
+ */
+int main () {
+    // TO DO...
+}
---
+++ AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso3.c
@@ -0,0 +1,20 @@
+/**
+ * @file Ripasso3.c
+ * @author your name 
+ * @brief Creare un file in C che crei un'immagine che stampi un triangolo equilatero di asterischi
+ * con base di dimensioni n (n deve essere dispari).
+ * Esempio con n = 7:
+ * 
+ *       *
+ *     * * *
+ *   * * * * *
+ * * * * * * * *
+ * 
+ * @version 0.1
+ * @date 2024-09-13
+ *
+ * 
+ */
+int main () {
+    // TO DO...
+}
