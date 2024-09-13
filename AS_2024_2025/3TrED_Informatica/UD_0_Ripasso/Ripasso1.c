caaf7c5d1bad0e417ecec12a69674bbd184f7df7
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-09-13 10:21:34 +0200

    add file C soluzioni

--- AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso1.c
+++ AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso1.c
@@ -1,7 +1,7 @@
 /**
  * @file Ripasso1.c
  * @author your name 
- * @brief Creare un file in C che prenda in input due valor interi A e B, con A < B.
+ * @brief Creare un file in C che prenda in input due valori interi A e B, con A < B.
  * Il programma deve stampare una matrice di AxB con tutti * .
  * @version 0.1
  * @date 2024-09-13
