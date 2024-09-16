b72a59d803b1187e9bfbf6a5e413353ddb6f6447
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-09-16 08:08:35 +0200

    add file C

--- AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso4.c
+++ AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso4.c
@@ -2,7 +2,7 @@
  * @file Ripasso4.c
  * @author your name 
  * @brief Crea un programma in C che stampi il perimetro di un rettandolo,
- * di dimensioni A e B (A <= B) date in input.
+ * di dimensioni A e B (A <= B), variabili date in input.
  * @version 0.1
  * @date 2024-09-13
  *
