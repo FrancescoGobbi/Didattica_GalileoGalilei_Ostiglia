98e0bbdf16e3fb3800535b7413cdcf8805cd5b27
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-09-17 09:01:44 +0200

    Add file C

--- AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso4.c
+++ AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso4.c
@@ -2,7 +2,13 @@
  * @file Ripasso4.c
  * @author your name 
  * @brief Crea un programma in C che stampi il perimetro di un rettandolo,
- * di dimensioni A e B (A <= B), variabili date in input.
+ * utilizzando solo asterischi di dimensioni A e B (A <= B), variabili date in input.
+ * Se A = 3 e B = 4
+ * 
+ *  * * * *
+ *  *     *
+ *  * * * *
+ * 
  * @version 0.1
  * @date 2024-09-13
  *
@@ -12,5 +18,23 @@
 #include <stdio.h>
 
 int main () {
-    // TO DO...
+    int A, B;
+    int i, j;
+
+    do{
+        printf("Inserisci due valori interi: ");
+        scanf("%d %d", &A, &B);
+    }while( A>B );
+
+    for(i=1; i<=A ; i++){ // Per le RIGHE
+        for(j=1; j<=B ; j++){ // Per le COLONNE
+            if(i==1 || i==A || j==1 || j==B) { // Per gli ASTERISCHI
+                printf(" * ");
+            }
+            else {
+                printf("   "); // Per gli SPAZIETTI
+            }
+        }
+        printf("\n");
+    }
 }
