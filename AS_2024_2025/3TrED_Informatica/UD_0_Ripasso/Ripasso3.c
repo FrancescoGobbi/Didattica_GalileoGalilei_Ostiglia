2e90e740d984cceb802371fb3fa20c5742c70cd2
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-09-17 08:53:08 +0200

    add file C

--- AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso3.c
+++ AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso3.c
@@ -2,7 +2,7 @@
  * @file Ripasso3.c
  * @author your name 
  * @brief Creare un programma in C che crei un'immagine che stampi un triangolo equilatero di asterischi
- * con base di dimensioni n (n deve essere dispari).
+ * con base di dimensioni n (con n> 0 AND dispari).
  * Esempio con n = 7:
  * 
  *       *
@@ -19,5 +19,33 @@
 #include <stdio.h>
 
 int main () {
-    // TO DO...
-}
+    int n, asterischi, spazietti; // Variabili utilizzate
+    int i, j, z; // Variabili per le iterazioni
+
+    do {
+        printf("Inserisci il valore di n: ");
+        scanf("%d", &n); // Prendo in input n
+    }while(n%2 == 0 || n<0);
+
+    // Inzializzo le variabili
+    asterischi = 1;
+    spazietti = (n-1)/2; // spazietti = n/2;
+
+    // Creo la figura
+    for(i=1 ; i<=(n/2 + 1) ; i++){ // Per le RIGHE
+        // Stampo gli spazietti
+        for(j=1; j<=spazietti  ; j++){ // Per gli SPAZIETTI
+            printf("   ");
+        }
+
+        // Stampo gli asterischi
+        for(z=1; z<=asterischi ; z++){// Per gli ASTERISCHI
+            printf(" * ");
+        }
+        // Preparo il tutto per andare nella nuova righe
+        printf("\n");
+        spazietti--; // Riduco gli SPAZIETTI
+        // Incremento gli ASTERISCHI
+        asterischi = asterischi + 2; // asterischi+=2;
+    } // End for RIGHE
+} 
