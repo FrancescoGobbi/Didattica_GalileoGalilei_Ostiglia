1cfae2169d5604f76a1642b340acbf9c9603bc00
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-09-18 23:03:45 +0200

    add file

--- AS_2024_2025/3TrED_Informatica/UD_1_Puntatori/Puntatori0.c
+++ AS_2024_2025/3TrED_Informatica/UD_1_Puntatori/Puntatori0.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    int x = 5; // Variabile intera
+    int a = 10; // Variabile intera
     // Con & si andrà a prendere l'indirizzo di memoria di una data variabile
-    printf("L'indirizzo di memoria della variabile x è: %p\n", &x);
-    printf("Il valore è: %d\n", x);
+    printf("L'indirizzo di memoria della variabile x è: %p\n", &a);
+    printf("Il valore è: %d\n", a);
 
     printf("\n\n");
 
-    // CREAZIONE DI UN PUNTATORE
-    int *y; // Creo una variabile puntatore ad interi
-    y = &x; // Inserisco il riferimento di memoria (indirizzo di memoria) di x dentro y
+    // CREAZIONE DI UN VARIABILE PUNTATORE
+    int *b; // Creo una variabile puntatore ad interi
+    b = &a; // Inserisco il riferimento di memoria (indirizzo di memoria) di x dentro y
     // Quindi ora y contiene l'indirizzo di memoria di x, ovvero che punta ad x.
+    // int *b = &a; // Riga alternativa con le due operazioni sopra inglobate in una sola
 
-    printf("Indirizzo di memoria di y: %p\n", &y);
-    printf("Valore contenuto in y: %p\n", y);
-    printf("Valore puntato da y: %d\n", *y); // *y è la deferenziazione della variabile puntatore
+    printf("Indirizzo di memoria di y: %p\n", &b);
+    printf("Valore contenuto in y: %p\n", b);
+    printf("Valore puntato da y: %d\n", *b); // *y è la deferenziazione della variabile puntatore
 
     printf("\n\n");
 
     // CREAZIONE DI UN PUNTATORE CHE PUNTA AD UN ALTRO PUNTATORE (DOPPIO PUNTATORE)
-    int **z; // Creo una variabile puntatore ad interi
-    z = &y; // Inserisco il riferimento di memoria (indirizzo di memoria) di y dentro z
+    int **c; // Creo una variabile puntatore ad interi
+    c = &b; // Inserisco il riferimento di memoria (indirizzo di memoria) di y dentro z
     // Quindi ora z contiene l'indirizzo di memoria di y, ovvero che punta ad y.
 
-    printf("Indirizzo di memoria di z: %p\n", &z);
-    printf("Valore contenuto in z: %p\n", z);
-    printf("Valore puntato da z: %p\n", *z); // *z è la deferenziazione della variabile puntatore
-    printf("Valore contenuto dentro *(*z): %d\n", *(*z)); // Doppio puntatore
+    printf("Indirizzo di memoria di z: %p\n", &c);
+    printf("Valore contenuto in z: %p\n", c);
+    printf("Valore puntato da z: %p\n", *c); // *z è la deferenziazione della variabile puntatore
+    printf("Valore contenuto dentro *(*z): %d\n", *(*c)); // Doppio puntatore
 }
