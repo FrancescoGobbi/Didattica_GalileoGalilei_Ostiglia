847905bea876e7fbf6f1cda1c95182fa35440d72
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-09-20 09:03:27 +0200

    Add File C

--- AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso5.c
+++ AS_2024_2025/3TrED_Informatica/UD_0_Ripasso/Ripasso5.c
@@ -21,5 +21,46 @@
 #include <stdio.h>
 
 int main () {
-    // TO DO...
+	int n, i, j;
+	char A, B;
+	
+    do{
+        printf("Inserisci un valore intero: ");
+        scanf("%d", &n);
+        fflush(stdin);
+    }while( n<=0 );
+    
+    // Vogliamo: Da 'A' a 'Z' e da 'a' a 'z'
+    // Ovvero:  (A>=65 && A<=90) || (A>=97 && A<=122)
+    // Altrimenti: (A>='A' && A<='Z') || (A>='a' && A<='z')
+    // Negazione(NOT --> !) della condizione composta/articolata sopra
+    // Quindi, NON vogliamo: (A<'A' || A>'Z') && (A<'a' || A>'z')
+    
+    //(Esempio: Voglio (x>=5 && x<=10) --> NON voglio: (x<5 || x>10))
+    do {
+	    printf("Inserisci un primo carattare: ");
+	    scanf("%c", &A);
+	    fflush(stdin);
+	}while((A<'A' || A>'Z') && (A<'a' || A>'z'));
+    
+    do {
+	    printf("Inserisci un secondo carattare: ");
+	    scanf("%c", &B);  
+		fflush(stdin); 
+	}while((B<'A' || B>'Z') && (B<'a' || B>'z'));
+
+    printf("Il valore di n �: %d\n", n);
+	printf("I due caratteri sono �: %c e %c\n", A, B);
+	
+	for(i = 1; i<=n; i++) { // Iterazione per le righe
+        for(j = 1; j<=n; j++) { // Iterazione per le colonne
+            if (i % 2 == 0) { // Righe pari
+                printf(" %c ",B); // Stampa del carattere contenuto nella variabile B
+            } 
+            else { // Righe dispari
+                printf(" %c ",A);
+            }
+        }
+        printf("\n"); // Vado a capo riga
+    }
 }
