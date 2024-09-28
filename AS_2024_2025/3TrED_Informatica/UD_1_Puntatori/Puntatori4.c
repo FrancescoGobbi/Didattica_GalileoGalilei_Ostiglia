547d24f9c775edd55a1abaa8263fa53c65b8235b
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-09-28 09:02:30 +0200

    Add file C

---
+++ AS_2024_2025/3TrED_Informatica/UD_1_Puntatori/Puntatori4.c
@@ -0,0 +1,25 @@
+/**
+ * @file Puntatori4.c
+ * @author your name 
+ * @brief Creare un file in C che prenda in input due valori interi A e B
+ * utilizzando i puntatori.
+ * Sempre utilizzando i puntatori eseguire la stampa di una rettandolo
+ * formato da '*' con dimensioni A x B.
+ * 
+ * ESEMPIO: Se A = 4 e B = 3
+ * L'output dovrà essere:
+ * * * *
+ * * * *
+ * * * *
+ * * * *
+ * 
+ * @version 0.1
+ * @date 2024-09-28
+ * 
+ */
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
