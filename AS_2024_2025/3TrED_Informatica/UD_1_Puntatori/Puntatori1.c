a80c0144185717b6041cbbaa7b8c3153fe2cb525
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-10-01 14:04:40 +0200

    Add file

--- AS_2024_2025/3TrED_Informatica/UD_1_Puntatori/Puntatori1.c
+++ AS_2024_2025/3TrED_Informatica/UD_1_Puntatori/Puntatori1.c
@@ -25,19 +25,18 @@ int main() {
     // Quindi ora p contiene l'indirizzo di memoria di a, ovvero che punta ad a.
     // int *p = &a; // Riga alternativa con le due operazioni sopra inglobate in una sola
 
-    printf("Indirizzo di memoria di p: %p\n", &p1);
-    printf("Valore contenuto in p: %p\n", p1);
-    printf("Valore puntato da p: %d\n", *p1); // *p1 è la deferenziazione della variabile puntatore
+    printf("Indirizzo di memoria di p1: %p\n", &p1);
+    printf("Valore contenuto in p1: %p\n", p1);
+    printf("Valore puntato da p1: %d\n", *p1); // *p1 è la deferenziazione della variabile puntatore
 
     printf("\n\n");
-
     // CREAZIONE DI UN PUNTATORE CHE PUNTA AD UN ALTRO PUNTATORE (DOPPIO PUNTATORE)
     int **p2; // Creo una variabile puntatore di puntatori, ovvero un puntatore che punta ad una variabile puntatori ad interi
     p2 = &p1; // Inserisco il riferimento di memoria (indirizzo di memoria) di p1 dentro p2
     // Quindi ora p2 contiene l'indirizzo di memoria di p1, ovvero che punta ad p1.
 
-    printf("Indirizzo di memoria di z: %p\n", &p2);
-    printf("Valore contenuto in z: %p\n", p2);
-    printf("Valore puntato da z: %p\n", *p2); // *p2 è la deferenziazione della variabile puntatore
+    printf("Indirizzo di memoria di p2: %p\n", &p2);
+    printf("Valore contenuto in p2: %p\n", p2);
+    printf("Valore puntato da p2: %p\n", *p2); // *p2 è la deferenziazione della variabile puntatore
     printf("Valore contenuto dentro *(*p2): %d\n", *(*p2)); // Doppio puntatore
 }
