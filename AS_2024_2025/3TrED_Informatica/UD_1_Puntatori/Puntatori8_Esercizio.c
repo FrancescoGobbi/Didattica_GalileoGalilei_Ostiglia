922adbb0c3174eb6e4e0954255cbfdcf9bf2f6d3
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-10-04 12:27:20 +0200

    Add file C

---
+++ AS_2024_2025/3TrED_Informatica/UD_1_Puntatori/Puntatori8_Esercizio.c
@@ -0,0 +1,14 @@
+/**
+ * @file Puntatori8_Esercizio.c
+ * @author your name
+ * @brief Crea un programma in C che, utilizzando solamente i puntatori,
+ * che prenda in input due valori interi (x e y) e questo deve
+ * calcolare il fattoriale del primo numero, utilizzando il puntatore anche per questo,
+ * e calcolare il fattoriale del secondo numero, utilizzando sempre il puntatore anche per questo.
+ * Deve confrontare il risualtato dei due fattoriali e stampare
+ * il valore maggiore
+ * 
+ * @version 0.1
+ * @date 2024-10-04
+ * 
+ */
