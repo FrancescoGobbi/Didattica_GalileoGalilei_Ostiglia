8524b684be067448c9899f001fb80e70e8a20aa6
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-10-04 11:57:27 +0200

    Add file C

---
+++ AS_2024_2025/3TrED_Informatica/UD_1_Puntatori/Puntatori7_Esercizio.c
@@ -0,0 +1,19 @@
+/**
+ * @file Puntatori7_Esercizio.c
+ * @author your name
+ * @brief Crea un programma in C che, utilizzando solamente i puntatori,
+ * prenda in input con 3 variabili (A, B e C) intere e scambi 
+ * i valori contentui nelle variabili tra loro, nel seguente modo:
+ * - La variabile B deve contenere il valore di A
+ * - La variabile C deve contenere il valore di B
+ * - La variabile A deve contenere il valore di C
+ * 
+ * ESEMPIO: 
+ * Se A = 5, B = 6 e C = 8
+ * Il programma deve fare:
+ * A = 8, B = 5 e C = 6
+ * 
+ * @version 0.1
+ * @date 2024-10-04
+ * 
+ */
