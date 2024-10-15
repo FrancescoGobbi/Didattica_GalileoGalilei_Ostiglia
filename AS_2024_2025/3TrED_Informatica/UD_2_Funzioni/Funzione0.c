982de4b7df78bc47621bf908617f98c7b2608b3e
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-10-15 17:29:27 +0200

    Add file C

---
+++ AS_2024_2025/3TrED_Informatica/UD_2_Funzioni/Funzione0.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Dichiarazione delle funzioni
+/**
+ * @brief Funzione che prende in input un valore e crea il suo quadrato
+ * 
+ * @return int 
+ */
+int quadrato (int);
+
+int main() {
+    int x; // Creo la varibile x nel main()
+    
+    // Prendo il valore della x
+    printf("Insersci un valore della x: ");
+    scanf("%d", &x);
+
+    // Stampo il valore della x
+    printf("Il valore della x è: %d\n", x);
+
+    // Eseguo il quadrato con la chiamata a funzione
+    int y = quadrato(x); // Chiamata della funzione quadrato()
+
+    // Stampa del quadrato
+    printf("(%d)^2 = %d\n", x, y);
+
+    // return 0;
+}
+
+// Definizione delle funzioni
+int quadrato (int x) {
+    int y = x * x;
+    return y; // return della funzione
+}
