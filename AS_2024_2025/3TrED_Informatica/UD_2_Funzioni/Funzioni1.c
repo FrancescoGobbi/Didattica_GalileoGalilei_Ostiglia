1394dee37e587d22d0e3023f79eb7ae921973c74
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-11-17 21:51:13 +0100

    Add file C

--- AS_2024_2025/3TrED_Informatica/UD_2_Funzioni/Funzioni1.c
+++ AS_2024_2025/3TrED_Informatica/UD_2_Funzioni/Funzioni1.c
@@ -42,12 +42,13 @@ int main() {
     // Chiamata delle funzioni stampaInt()
     stampaInt(x);
     stampaInt(y);
-    printf("Sono nella funzione main: x=%p y=%p\n", &x,&y);
+    printf("\n\nSono nella funzione main(): x=%p y=%p\n", &x,&y);
 
-    Printf("\n\nSono tornato nel main() e devo chiamare sommaVar() per sommare due valori.\n");
+    printf("\n\nSono tornato nel main() e devo chiamare sommaVar() per sommare due valori.\n");
     int somma = sommaVar(x,y);
     stampaInt(somma);
 
+    printf("\n\nSono tornato nella funzione main().\n");
     // Funzioni già viste a lezione
     printf("Inserisci due valori interi: ");
     scanf("%d%d", &x, &y);
@@ -59,30 +60,36 @@ int main() {
     // Funzione che prende in input la base, come primo argomento
     // e l'esponente come secondo argomento 
     // Esegue la potenza della base elevata all'esponente
-    // Il risultato/return è un float 
+    // Il risultato/return è un float
+    printf("\n\nSono tornato nella funzione main().\nFaccio la l'operazione %d^%d\n", x, y); 
     float potenza = pow(x, y); 
     printf("%d^%d = %.2f\n", x, y, potenza);
 
     // Potenza di una potenza (funzione con all'interno un'altra funzione)
+    printf("\n\nSono tornato nella funzione main().\nFaccio la l'operazione (2^4)^5\n"); 
     float val = pow(pow(2,4),5);
     printf("%f\n", val);
 
     // Funzione void
+    printf("\n\nSono tornato nella funzione main().\nChiedo in input un numero per verificare se è un numero primo o no.\n"); 
     printf("Inserisci un numero da definire se è primo: ");
     scanf("%d", &x);
     numeroPrimo(x);
+    printf("\n\nSono tornato nella funzione main().\nVerifico ora se 15 è un numeroprimo o no.\n"); 
     x = 15;
     numeroPrimo(x);
     
 } // Fine main()
 
 void stampaInt(int a) {
-    printf("\n\nSono nella funzione stampoInt().\nDevo stampareil numero %d.\n", a);
-    printf("Il numero intero è: %d\n", a);
+    printf("\n\nSono nella funzione stampoInt().\nDevo stampare il numero %d.\n", a);
+    printf("Il numero intero è a=%d\n", a);
+    printf("Sono nella funzione stampaInt(): a=%p\n", &a);
 }
 
 int sommaVar(int x, int y) {
     printf("\n\nSono nella funzione sommaVar().\nDevo sommare i valori: x=%d y=%d\n", x,y);
+    printf("Sono nella funzione sommaVar(): x=%p y=%p\n", &x,&y);
     int s = x + y;
     return s; // return x+y;
 }
