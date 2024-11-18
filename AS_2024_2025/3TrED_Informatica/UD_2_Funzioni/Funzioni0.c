d076e3a6678af6b227059cf9e43dcbceb801aaa0
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-11-18 14:29:08 +0100

    add file C

--- AS_2024_2025/3TrED_Informatica/UD_2_Funzioni/Funzioni0.c
+++ AS_2024_2025/3TrED_Informatica/UD_2_Funzioni/Funzioni0.c
@@ -28,14 +28,16 @@ int main() {
     printf("(%d)^2 = %d\n", x, y);
     printf("Indirizzo di memoria della variabile y del main(): %p\n", &y);
 
-    // return 0;
+    // return 0; // Return della funzione main()
 }
 
 // Definizione delle funzioni
 int quadrato (int x) {
+    // La variabile x è una variabile che la funzione quadrato()
+    // La variabile x è una variabile presa in input dalla funzione che l'ha chiamata
     printf("\n\nSono nella funzione quadrato().\n");
     printf("Indirizzo di memoria della variabile x della funzione quadrato(): %p\n", &x);
-    int y = x * x;
+    int y = x * x; // Variabile y della funzione quadrato()
     printf("Indirizzo di memoria della variabile y della funzione quadrato(): %p\n", &y);
-    return y; // return della funzione
+    return y; // return della funzione quadrato()
 }
