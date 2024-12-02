579bb3dd9d41c54c794709534438978426844d3a
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-12-02 21:56:46 +0100

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/C_Programmazione00.c
@@ -0,0 +1,10 @@
+// Importiamo la libreria standard per input e output
+#include <stdio.h>
+
+// Definiamo la funzione principale del programma
+int main() {
+    // printf() --> Funzione per la stampa a schermo (dare in output all'utente)
+    // Messaggio tra doppie virgolette --> Messaggio che si vuole stampare
+    // \n --> Utilizzato per andare a capo riga
+    printf("Ciao Mondo!\n");
+}
---
+++ AS_2024_2025/3TrED_Informatica/UD_2_Funzioni/Funzioni5.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * @brief Funzione che restituisce il massimo dati due numeri interi in input
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
+int maggiore(int a, int b);
+
+/**
+ * @brief Funzione che stampa i primi n valori interi partendo da 0, dato il valore di n in input
+ * 
+ * @param n 
+ */
+void stampaNumeri(int n);
+
+/**
+ * @brief Funzione che eleva un numero al quadrato e restituisce il risultato tramite un puntatore.
+ * 
+ * @param numero 
+ * @param *risultato 
+ */
+void elevaAlQuadrato(int numero, int *risultato);
+
+/**
+ * @brief Funzione che prende in input un numero n, considerando che questo sia in binario.
+ * La funzione calcola e restituisce il numero decimale associato
+ * 
+ * @param n 
+ * @return int 
+ */
+int convertiInDecimale(int n);
+
+/**
+ * @brief Funzione che prende in input un numero n, considerando che sia intero e positivo.
+ * La funzione calcola il binario del numero e lo restituisce.
+ * N.B. Deve restituire un velore in binario (di soli 0 ed 1) in una variabile intera
+ * 
+ * @param n 
+ * @return int 
+ */
+int convertiInBinario(int n);
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int quadrato;
+    int binario = 1111000; // Intero con un numero "binario", ovvero 120, per noi binario...
+
+    // Chiamata della funzione maggiore()
+    // Chiamo la funzione dentro la funzione printf(). Quindi stampo, senza salvare, il valore del return
+    printf("Il valore maggiore tra %d e %d è: %d\n", a, b, maggiore(a, b));
+
+    // Chiamata della funzione stampaNumeri()
+    stampaNumeri(a);
+
+    // Chiamata della funzione elevaAlQuadrato()
+    elevaAlQuadrato(a, quadrato);
+    printf("%d^2 = %d\n", a, quadrato);
+
+    // Chiamata della funzione convertiInDecimale()
+    int decimale = convertiInDecimale(binario);
+    printf("Il valore %d = %d\n", binario, decimale);
+
+    // Chiamata della funzione convertiInBinario()
+    binario = convertiInBinario(decimale);
+    printf("Il valore %d = %d\n", decimale, binario);
+
+}
+
+int maggiore(int a, int b) {
+    // TO DO...
+}
+
+void stampaNumeri(int n) {
+    // TO DO..
+}
+
+void elevaAlQuadrato(int numero, int *risultato){
+    // TO DO...
+}
+
+int convertiInDecimale(int n){
+    // TO DO...
+}
+
+int convertiInBinario(int n){
+    // TO DO...
+}
