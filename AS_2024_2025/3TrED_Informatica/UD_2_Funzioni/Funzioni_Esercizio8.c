b7f092726b24442166d7fe0181d568bc05c64374
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-12-09 12:53:44 +0100

    Add file C

---
+++ AS_2024_2025/3TrED_Informatica/UD_2_Funzioni/Funzioni_Esercizio8.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * @brief Funzione che calcola e rescituisce il fattoriale di n.
+ * 
+ * @param n 
+ * @return int 
+ */
+int fattoriale(int n);
+
+/**
+ * @brief Funzione che calcola e restituisce la media tra tre valori in input alla funzione.
+ * 
+ * @param a 
+ * @param b 
+ * @param c 
+ * @return flaot 
+ */
+float media(int a, int b, int c);
+
+/**
+ * @brief Funzione che verifica se un numero, passato per riferimento è primo o no.
+ * 
+ * @param n 
+ */
+void numeroPrimo(int *n);
+
+int main() {
+    int a = 5;
+    int b = 4;
+    int c = 10;
+
+    // Chimare la funzione fattoriale()
+    // TO DO...
+
+    // Chiamare la funzione media()
+
+
+    // Chiamare la funzione numeroPrimo()
+}
+
+int fattoriale(int n) {
+    // TO DO...
+}
+
+float media(int a, int b, int c) {
+    // TO DO...
+}
+
+void numeroPrimo(int *n) {
+    // TO DO...
+}
