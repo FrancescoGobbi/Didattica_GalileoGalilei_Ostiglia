3d568b83240d7519dfa64f0ee8c07e0f92d45a33
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-12-10 07:46:37 +0100

    Add fil C

---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/C_Programmazione02.c
@@ -0,0 +1,16 @@
+// Importiamo la libreria standard per input e output
+#include <stdio.h>
+
+// Definiamo la funzione principale del programma
+int main() {
+    // Dichiarazione di due variabili di tipo intero (4 byte)
+    int x = 5;
+    int y = 6;
+
+    printf("Il primo numero è %d, il secondo numero è %d\n", x, y);
+
+    // Dichiarazione di due variabili di tipo intero (4 byte)
+    int c = 5, d = 6;
+
+    printf("Il primo numero è %d, il secondo numero è %d\n", c, d);
+}
