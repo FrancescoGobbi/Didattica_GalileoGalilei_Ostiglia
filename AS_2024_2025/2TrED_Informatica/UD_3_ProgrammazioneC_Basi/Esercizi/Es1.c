fdd210f6c082f3bda873d38b2deced18be186000
FrancescoGobbi <francesco.gobbi4@gmail.com>
2024-12-17 16:22:02 +0100

    Add file esercizi

---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es1.c
@@ -0,0 +1,7 @@
+/* Es1
+Creare un programma in C che crei 4 variabili intere, da assegnare voi il valore.
+Creare la media tra i quatro valori e stampare il risultato.
+*/
+
+#include <stdio.h>
+// TO DO...
---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es2.c
@@ -0,0 +1,11 @@
+/* Es4
+Creare un programma in C che crei 4 variabili floar, da assegnare voi il valore.
+Creare la media tra i quatro valori e stampare il risultato.
+
+N.B. Verificare il risultato con l'esercizio precedente.
+Rispetto a come questi due stampano il risultato, ovvero la media, magari utilizzando proprio
+gli stessi valori assegnati alle 4 variaibili.
+*/
+
+#include <stdio.h>
+// TO DO...
