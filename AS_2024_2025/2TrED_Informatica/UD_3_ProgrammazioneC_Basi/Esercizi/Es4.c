5fc336565b79dacd1e10858635bdefbf38c1e6cd
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-01-07 12:02:52 +0100

    Add file Esercizi

---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es4.c
@@ -0,0 +1,7 @@
+/* Es4
+Crea un programma in C che prenda in input due valori interi.
+Il programma deve eseguire tutte le operazioni tra il primo valore ed il secondo.
+Il programma deve stampare i risultati.
+*/
+
+#include <stdio.h>
---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es5.c
@@ -0,0 +1,6 @@
+/* Es5
+Crea un programma in C che prenda un valore intero, uno float, uno double ed uno char.
+Il programma deve stampare i valori inseriti.
+*/
+
+#include <stdio.h>
