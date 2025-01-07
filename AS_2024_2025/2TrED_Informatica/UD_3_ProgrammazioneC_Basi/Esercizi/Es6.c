f01b9758072275d8d0171d09d0ee28c56dbbb2d6
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-01-07 21:59:21 +0100

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es6.c
@@ -0,0 +1,7 @@
+/* Es6
+Crea un programma in C che prenda in input due valori interi (a e b).
+Il programma deve eseguire tutte le operazioni tra a e b e stampare i risultati.
+(Almeno le operazioni basilari come: somma, differenza, prodotto e divizione)
+*/
+
+#include <stdio.h>
