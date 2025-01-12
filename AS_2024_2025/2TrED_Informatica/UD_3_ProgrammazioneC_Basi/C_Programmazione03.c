c29f0683d346449aa48997fa6573d8a9a856232e
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-01-12 23:46:57 +0100

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/C_Programmazione03.c
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/C_Programmazione03.c
@@ -22,6 +22,7 @@ int main() {
     // %d specifica il formato per un intero, &numero_intero è la variabile in cui viene memorizzato
     printf("Inserisci un numero intero: ");
     scanf("%d", &numero_intero);
+    fflush(stdin);
 
     // Richiesta di input per il numero decimale (float)
     // scanf() è una funzione standard di libreria <stdio.h> che richiede 
@@ -29,6 +30,7 @@ int main() {
     // %f specifica il formato per un float, &numero_decimale è la variabile in cui viene memorizzato
     printf("Inserisci un numero decimale (float): ");
     scanf("%f", &numero_decimale);
+    fflush(stdin);
 
     // Richiesta di input per il carattere
     // scanf() è una funzione standard di libreria <stdio.h> che richiede 
@@ -36,7 +38,8 @@ int main() {
     // %c specifica il formato per un carattere singolo, &carattere è la variabile in cui viene memorizzato
     // Lo spazio prima di %c serve per ignorare il carattere di invio rimasto nel buffer
     printf("Inserisci un carattere: ");
-    scanf(" %c", &carattere);
+    scanf("%c", &carattere);
+    fflush(stdin);
 
     // Richiesta di input per il numero double
     // scanf() è una funzione standard di libreria <stdio.h> che richiede 
@@ -44,6 +47,7 @@ int main() {
     // %lf specifica il formato per un double, &numero_grande è la variabile in cui viene memorizzato
     printf("Inserisci un numero decimale grande (double): ");
     scanf("%lf", &numero_grande);
+    fflush(stdin);
 
     // Stampa del valore di ogni variabile
     // %d specifica il formato per un intero, numero_intero è il valore da stampare
---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/C_Programmazione04.c
@@ -0,0 +1,33 @@
+/*
+Programma per introduzione delle selzioni in C, con if ed else.
+Questo contrutto è uguale al rombo nei diagrammi a blocchi, dove il vero è
+all'interno delle parentesi graffe dell'if, mentre il falso nelle parentesi
+graffe dell'else.
+
+Il programma verifica se un numero in input è positivo o negativo.
+*/
+
+#include <stdio.h>
+
+int main() {
+    // Dichiarazione delle variabili
+    int numero;
+
+    // Spiegazione all'utente su cosa fare
+    printf("Inserisci un numero intero: ");
+
+    // Richiesta in input all'utente del numero intero 
+    scanf("%d", &numero);
+
+    // Inizio della struttura/costrutto di selezione if-else
+    // Viene valutata la condizione all'interno delle parentesi tondo dopo l'if.
+    // Se la condizione è vera viene eseguito il codice tra parentesi graffe successive,
+    // altrimenti viene eseguito il codice tra le parentesi graffe dell'else.
+    if (numero >= 0) { // VERO
+        // Questo blocco viene eseguito se il numero è maggiore di 0
+        printf("Il numero %d è positivo.\n", numero);
+    } else { // FALSO
+        // Questo blocco viene eseguito se il numero è minore di 0
+        printf("Il numero %d è negativo.\n");
+    }
+}
