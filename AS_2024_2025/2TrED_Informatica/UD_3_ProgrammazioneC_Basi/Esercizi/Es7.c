9d57cb5f8cfda932b847a7f1af50296fa0e6e462
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-01-14 12:16:38 +0100

    Add file esercizi

---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es7.c
@@ -0,0 +1,11 @@
+/* Es7
+Crea un programma in C che prenda in input due valori interi (a e b).
+Il programma deve verificare se entrambi i numeri sono maggiori di 50.
+Se SI, stampare "ok", altrimenti "no".
+*/
+
+#include <stdio.h>
+
+int main() {
+   // TO DO...
+}
---
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es8.c
@@ -0,0 +1,11 @@
+/* Es8
+Crea un programma in C che prenda in input un valore intero a.
+Il programma deve verificare se il valore è compreso tra 10 e 50.
+Se SI, stampare "ok", altrimenti "no".
+*/
+
+#include <stdio.h>
+
+int main() {
+   // TO DO...
+}
