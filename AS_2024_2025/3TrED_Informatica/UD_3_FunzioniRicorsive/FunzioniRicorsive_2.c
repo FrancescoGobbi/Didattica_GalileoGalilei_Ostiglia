64e89294cc7cb61e2bc65a7b18a4fe5630af73d4
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-01-14 11:15:19 +0100

    Add file C

--- AS_2024_2025/3TrED_Informatica/UD_3_FunzioniRicorsive/FunzioniRicorsive_2.c
+++ AS_2024_2025/3TrED_Informatica/UD_3_FunzioniRicorsive/FunzioniRicorsive_2.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+/*
+Creare una funzione ricorsiva in C che prenda in input un numero intero n.
+La funzione deve eseguire la somma ricorsiva dei primi n numeri interi.
+*/
+int sommaN(int n);
+
+/*
+Creare una funzione ricorsiva in C che prenda in input un numero intero n.
+La funzione deve stamapre in ordine decrescente gli n valori che vanno da 1 ad n.
+
+ESEMPIO: Se n = 6, si dovrà stampare: 6  5  4  3  2  1
+*/
 void stampaNDec(int n);
 
+/*
+Creare una funzione ricorsiva in C che prenda in input un numero intero n.
+La funzione deve stamapre in ordine crescente gli n valori che vanno da 1 ad n.
 
+ESEMPIO: Se n = 6, si dovrà stampare: 1  2  3  4  5  6
+*/
 void stampaNCre(int n);
 
 int main() {
@@ -12,11 +29,24 @@ int main() {
     printf("Inserisci un numero intero: ");
     scanf("%d", &numero);
 
+    int somma = sommaN(numero);
+    printf("La somma dei primi n numeri è: %d\n", somma);
+    printf("\n\n\n");
+
     stampaNDec(numero);
     printf("\n\n\n");
 
     stampaNCre(numero);
     printf("\n\n\n");
+}
+
+int sommaN(int n) {
+    if (n == 1) { // Caso base
+        return 1;
+    }
+    else { // Caso ricorsivo
+        return n + sommaN(n - 1);
+    }
 }
 
 void stampaNDec(int n) {
