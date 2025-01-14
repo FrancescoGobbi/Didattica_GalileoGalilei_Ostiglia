2a8f405fe228ec1cba49c07a4e3ba39ecc709977
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-01-14 10:08:58 +0100

    Add file

--- AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es4.c
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es4.c
@@ -5,3 +5,33 @@ Il programma deve stampare i risultati.
 */
 
 #include <stdio.h>
+
+int main() {
+    // Creo le variabili
+    int a, b;
+    int somma, differenza, moltiplicazione, divisione;
+
+    // Richiedo in input i valori
+    printf("Inserisci il primo valore intero: ");
+    scanf("%d", &a);
+    printf("Inserisci il secondo valore intero: ");
+    scanf("%d", &b);
+
+    // Soluzione condensata dei due input
+    /*
+    printf("Inserisci i due valori interi: ");
+    scanf("%d %d", &a, &b); // Prendo i due valori con una sola riga di codice
+    */
+
+    // Eseguo le operazioni 
+    somma = a + b;
+    differenza = a - b;
+    moltiplicazione = a * b;
+    divisione = a / b;
+
+    // Stampo i risultati
+    printf("%d + %d = %d\n", a, b, somma);
+    printf("%d - %d = %d\n", a, b, differenza);
+    printf("%d * %d = %d\n", a, b, moltiplicazione);
+    printf("%d / %d = %d\n", a, b, divisione);
+}
--- AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es5.c
+++ AS_2024_2025/2TrED_Informatica/UD_3_ProgrammazioneC_Basi/Esercizi/Es5.c
@@ -4,3 +4,34 @@ Il programma deve stampare i valori inseriti.
 */
 
 #include <stdio.h>
+
+int main() {
+    // Creo le variabili
+    int a;
+    float b;
+    double c;
+    char d;
+
+    // Prendo in input i valori
+    printf("Inserisci un valore intero: ");
+    scanf("%d", &a);
+    fflush(stdin); // Consigliato dopo ogni richiesta in input
+
+    printf("Inserisci un valore float: ");
+    scanf("%f", &b);
+    fflush(stdin); // Consigliato dopo ogni richiesta in input
+
+    printf("Inserisci un valore double: ");
+    scanf("%f", &c);
+    fflush(stdin); // Consigliato dopo ogni richiesta in input
+
+    printf("Inserisci un carattere char: ");
+    scanf("%c", &d);
+    fflush(stdin); // Consigliato dopo ogni richiesta in input
+
+    // Stampo i valori inseriti
+    printf("Il valore intero è: %d\n", a);
+    printf("Il valore float è: %f\n", b);
+    printf("Il valore double è: %f\n", c);
+    printf("Il valore char è: %c\n", d);
+}
