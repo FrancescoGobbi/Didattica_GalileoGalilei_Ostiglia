3f806e609d99095fa2557af3518b3715a0ac11ae
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-01-28 10:31:09 +0100

    Add

---
+++ AS_2024_2025/2TrED_Informatica/UD_4_ProgrammazioneC_Controlli_Iterazioni/C_Condizione_0.c
@@ -0,0 +1,25 @@
+/*
+Condizione 0
+Creare un programma in C che prenda in input da parte dell'utente un valore intero (numero).
+Il programma deve verificare se il numero è pari o dispari.
+Se il numero è pari, il programma deve stampare "Il numero X è pari."
+Se il numero è dispari, il programma deve stampare "Il numero X è dispari."
+*/
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    // Input del numero
+    printf("Inserisci un numero: ");
+    scanf("%d", &numero);
+
+    // Verifica se il numero è pari o dispari
+    if (numero % 2 == 0) { // Modulo (%) = resto della divisione
+        printf("Il numero %d è pari.\n", numero);
+    } 
+    else {
+        printf("Il numero %d è dispari.\n", numero);
+    }
+}
