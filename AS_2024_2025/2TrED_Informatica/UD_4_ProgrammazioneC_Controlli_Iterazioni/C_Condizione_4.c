be43b16b5a0ab86dcd844891744558cbd52d60e3
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-04 11:05:01 +0100

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_4_ProgrammazioneC_Controlli_Iterazioni/C_Condizione_4.c
+++ AS_2024_2025/2TrED_Informatica/UD_4_ProgrammazioneC_Controlli_Iterazioni/C_Condizione_4.c
@@ -20,4 +20,43 @@ int main() {
     scanf("%d", &C);
 
     // TO DO...
+    // Soluzione 1 (Abbastanza corretta)
+    if (A > B && A > C) {
+        printf("Il numero A: %d è il maggiore\n", A);
+    }
+    if (B > A && B > C) {
+        printf("Il numero B: %d è il maggiore\n", B);
+    }
+    if (C > A && C > B) {
+        printf("Il numero C: %d è il maggiore\n", C);
+    }
+
+    // Soluzione 2 (Corretta con if annidati)
+    if (A > B) {
+        if (A > C) {
+            printf("Il numero A: %d è il maggiore\n", A);
+        }
+        else {
+            printf("Il numero C: %d è il maggiore\n", C);
+        }
+    }
+    else {
+        if (B > C){
+            printf("Il numero B: %d è il maggiore\n", B);
+        }
+        else {
+            printf("Il numero C: %d è il maggiore\n", C);
+        }
+    }
+
+    // Soluzione 3 (Corretta con else-if, condizioni ed utilizzo di oepratori logici)
+    if (A > B && A > C) {
+        printf("Il numero A: %d è il maggiore\n", A);
+    }
+    else if (B > A && B > C) {
+        printf("Il numero B: %d è il maggiore\n", B);
+    }
+    else {
+        printf("Il numero C: %d è il maggiore\n", C);
+    }
 }
