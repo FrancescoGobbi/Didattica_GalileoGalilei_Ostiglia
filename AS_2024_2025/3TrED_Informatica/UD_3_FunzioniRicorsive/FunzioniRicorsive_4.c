7f24863b5e186565361efa26a81dca2943dcb4f9
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-05 14:00:16 +0100

    Add file C

---
+++ AS_2024_2025/3TrED_Informatica/UD_3_FunzioniRicorsive/FunzioniRicorsive_4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Creare una funzione ricorsiva in C che prenda in input un numero intero n.
+La funzione deve contare e restituire il numero dei numeri pari tra n ed 1.
+*/
+int contaPari(int n);
+
+/*  Creare una funzione ricorsiva in C che prenda in input un numero n ed un
+numero m.
+La funzione deve sommare e restituire la somma di tutti i numeri pari tra 1 e n 
+sommata a quella tra 1 ed m.
+*/
+int sommaPari(int n, int m);
+
+/*
+Creare una funzione ricorsiva in C che prenda in input un numero a ed un
+numero b.
+La funzione deve restituire il risultato della divisione tra a e b, utilizzando
+la ricorsione.
+*/
+int divisione(int a, int b);
+int main() {
+
+}
