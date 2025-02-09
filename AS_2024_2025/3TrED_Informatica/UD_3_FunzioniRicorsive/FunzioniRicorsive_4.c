29222bcea2940c790ade180b18077ace31343bfb
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-09 22:26:58 +0100

    Add fil C

--- AS_2024_2025/3TrED_Informatica/UD_3_FunzioniRicorsive/FunzioniRicorsive_4.c
+++ AS_2024_2025/3TrED_Informatica/UD_3_FunzioniRicorsive/FunzioniRicorsive_4.c
@@ -21,5 +21,32 @@ la ricorsione.
 int divisione(int a, int b);
 
 int main() {
+    int n = 5, m = 6;
 
+    printf("Esercizio di conta dei numeri pari\n");
+    int pari = contaPari(n);
+    printf("Il numero di numeri pari tra 1 e %d è: %d\n", n, pari);
+    int pari2 = contaPari(m);
+    printf("Il numero di numeri pari tra 1 e %d è: %d\n", m, pari2);
+
+    printf("\n\n\nEsercizio della SOMMA dei numeri pari\n");
+    int somma = sommaPari(n, m);
+    printf("La somma dei numeri pari tra 1 e %d e tra 1 e %d è: %d\n", n, m, somma);
+
+    printf("\n\n\nEsercizio della DIVISIONE\n");
+    int a = 10, b = 2;
+    int div = divisione(a, b);
+    printf("La divisione tra %d e %d è: %d\n", a, b, div);
+}
+
+int contaPari(int n) {
+    // TO DO...
+}
+
+int sommaPari(int n, int m){
+    // TO DO...
+}
+
+int divisione(int a, int b){
+    // TO DO...
 }
