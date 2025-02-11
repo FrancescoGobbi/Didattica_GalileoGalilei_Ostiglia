2856c5a00b84021299063f10d43f0e63d805d31a
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-11 10:57:21 +0100

    add file C

--- AS_2024_2025/2TrED_Informatica/UD_4_ProgrammazioneC_Controlli_Iterazioni/C_Condizione_5.c
+++ AS_2024_2025/2TrED_Informatica/UD_4_ProgrammazioneC_Controlli_Iterazioni/C_Condizione_5.c
@@ -9,5 +9,48 @@ Se tutti i numeri sono diversi, il programma deve stampare "Tutti i numeri sono 
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    // Creo le variabili
+    int a, b, c;
+
+    // Prendo in input le variabili
+    printf("Insersci il valore per A: ");
+    scanf("%d", &a);
+    printf("Insersci il valore per B: ");
+    scanf("%d", &b);
+    printf("Insersci il valore per C: ");
+    scanf("%d", &c);
+
+    // Faccio le varie considerazioni/controlli (PRIMA SOLUZIONE)
+    if (a == b) {
+        printf("Almeno due numeri sono uguali.\n");
+    }
+    else if(b == c) {
+        printf("Almeno due numeri sono uguali.\n");
+    }
+    else if (a == c) {
+        printf("Almeno due numeri sono uguali.\n");
+    }
+    else {
+        printf("Tutti i numeri sono diversi.\n");
+    }
+
+    /* Soluzione scorretta
+    if (a == b) {
+        printf("Almeno due numeri sono uguali.\n");
+    }
+    if (b == c) {
+        printf("Almeno due numeri sono uguali.\n");
+    }
+    if (a == c) {
+        printf("Almeno due numeri sono uguali.\n");
+    }
+    */
+
+    // Faccio le varie considerazioni/controlli (SECONDA SOLUZIONE)
+    if (a == b || b == c || a == c){
+        printf("Almeno due numeri sono uguali.\n");
+    }
+    else {
+        printf("Tutti i numeri sono diversi.\n");
+    }
 }
