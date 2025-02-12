08be9ba0e6f912f972edbf978788c497283f6af9
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-12 13:26:18 +0100

    Add fil mysql

---
+++ AS_2024_2025/5IA_Informatica/Lezioni/Esercizi_ModellazioneConcettuale/Esercizio05/ql.sql
@@ -0,0 +1,17 @@
+-- Q1 : Trovare tutti i nomi ed i cognomi dei docenti
+SELECT D.nome AS 'NomeDocente', D.cognome AS 'CognomeDocente'
+FROM Docente AS D
+
+-- Q2 : Trovare tutti i nomi ed i cognomi degli Studenti
+
+
+-- Q3 : Trovare tutti i nomi ed i cognomi degli Studenti che frequentano il corso di Informatica
+
+
+-- Q4 : Trovare tutti i nomi ed i cognomi degli Studenti che frequentano il corso di Informatica e sono iscritti al corso di Matematica
+
+
+-- Q5 : Trovare tutti i nomi ed i cognomi dei docenti che fanno o hanno fatto un corso di Matematica
+
+
+-- Q6 : Trovare tutti i ragazzi che si sono iscritti prima del 2024-10-02
