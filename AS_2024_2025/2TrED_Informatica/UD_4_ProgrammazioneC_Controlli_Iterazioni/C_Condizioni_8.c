ec32e4f7ac00c65b942ce3b5c3cb42adeca411b6
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-16 21:28:29 +0100

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_4_ProgrammazioneC_Controlli_Iterazioni/C_Condizioni_8.c
@@ -0,0 +1,11 @@
+/*
+Condizione 8
+Creare un programma in C che prenda in input da parte dell'utente due valori interi (A e B).
+Il programma deve verificare se almeno se A è divisibile per B.
+Se A è divisibile per B, il programma deve stampare "A è divisibile per B."
+Se A non è divisibile per B, il programma deve stampare "A non è divisibile per B."
+*/
+
+int main() {
+    // TO DO...
+}
