63f6a7904a7212396dd6ecc1f78721f8912c6a82
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-16 21:30:52 +0100

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_4_ProgrammazioneC_Controlli_Iterazioni/C_Condizione_9.c
@@ -0,0 +1,15 @@
+/*
+Condizione 9
+Creare un programma in C che prenda in input da parte dell'utente tre valori interi (A, B e C).
+Il programma deve verificare se i tre numeri possono formare i lati di un triangolo.
+Se i numeri possono formare un triangolo, il programma deve determinare il tipo di triangolo:
+- Equilatero (tutti i lati uguali)
+- Isoscele (due lati uguali)
+- Scaleno (tutti i lati diversi)
+Il programma deve stampare il tipo di triangolo.
+Se i numeri non possono formare un triangolo, il programma deve stampare "I numeri non possono formare un triangolo."
+*/
+
+int main() {
+    // TO DO...
+}
