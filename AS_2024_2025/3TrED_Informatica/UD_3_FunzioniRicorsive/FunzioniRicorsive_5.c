f6044c934a940ee35fbcc99b413dd6c2ceb7d66c
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-17 13:41:24 +0100

    Add file C

--- AS_2024_2025/3TrED_Informatica/UD_3_FunzioniRicorsive/FunzioniRicorsive_5.c
+++ AS_2024_2025/3TrED_Informatica/UD_3_FunzioniRicorsive/FunzioniRicorsive_5.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int fibonacci(int n);
+
 /*
 Creare una funzione sommaFibonacci che prenda in input due valori interi a e b.
 La funzione deve restituire la somma tra il numero di Fibonacci di a e il numero di Fibonacci di b.
@@ -31,14 +33,42 @@ int main() {
     printf("L'MCD tra %d e %d è: %d\n", a, b, mcd);
 }
 
-int sommaFibonacci(int a , int b){
-    // TO DO...
+int fibonacci(int n) {
+
+    // Caso base: se n è 0 o 1, ritorna n
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    else {
+        // Caso/Passo ricorsivo: somma dei due numeri precedenti della sequenza
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
-float MCD(int a, int b) {
-    // TO DO...
+int sommaFibonacci(int a, int b) {
+    // Calcola la somma dei numeri di Fibonacci in posizione a e b
+    return fibonacci(a) + fibonacci(b);
 }
 
-void stampaInversaNumero(int n){
-    // TO DO...
+int MCD(int a, int b) {
+    // Caso base: se b è 0, ritorna a
+    if (b == 0) {
+        return a;
+    }
+    else {
+        // Caso/Passo ricorsivo: chiama MCD con b e il resto della divisione di a per b
+        return MCD(b, a % b);
+    }
+}
+
+void stampaInversaNumero(int n) {
+    // Caso base: se n è 0, termina la ricorsione
+    if (n == 0) {
+        return;
+    }
+    else {
+        // Caso/Passo ricorsivo: stampa l'ultima cifra di n e chiama la funzione con n diviso per 10
+        printf("%d ", n % 10);
+        stampaInversaNumero(n / 10);
+    }
 }
