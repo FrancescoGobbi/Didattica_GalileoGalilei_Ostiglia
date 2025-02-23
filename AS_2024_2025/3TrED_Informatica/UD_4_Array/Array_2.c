2304dc459bf38c2d1e1e6cca5456feb1048cbf14
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-23 23:00:58 +0100

    Add file C

---
+++ AS_2024_2025/3TrED_Informatica/UD_4_Array/Array_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+Creare il codice in C di un programma che crea un array di dimensine 10.
+L'array deve essere riempito con i primi 10 numeri della sequenza di Fibonacci.
+Infine, il programma deve stampare gli elementi dell'array.
+*/
+
+int main() {
+    // TO DO...
+}
