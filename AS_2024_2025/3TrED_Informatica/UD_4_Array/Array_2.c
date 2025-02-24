7a1ce0c2c297dec022eb0730e648bc732e38fac6
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-24 14:01:21 +0100

    Add file C

--- AS_2024_2025/3TrED_Informatica/UD_4_Array/Array_2.c
+++ AS_2024_2025/3TrED_Informatica/UD_4_Array/Array_2.c
@@ -6,6 +6,20 @@ L'array deve essere riempito con i primi 10 numeri della sequenza di Fibonacci.
 Infine, il programma deve stampare gli elementi dell'array.
 */
 
+#define dim 10
+
 int main() {
-    // TO DO...
+    int i;
+    int vetFib[10] = {0, 1}; // Creiamo il vettore di Fibonacci con i casi base ed il resto a 0
+
+    for(i = 2; i < dim; i++) {
+        vetFib[i] = vetFib[i-1] + vetFib[i-2];
+    }
+
+    printf("Vettore di Fibonacci: ");
+
+    for(i = 0; i < dim; i++) {
+        printf("%3d", vetFib[i]);
+    }
+    printf("\n");
 }
