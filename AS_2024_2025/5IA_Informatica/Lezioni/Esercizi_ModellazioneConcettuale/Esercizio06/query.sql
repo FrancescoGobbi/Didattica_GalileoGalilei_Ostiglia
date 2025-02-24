5c83176f15cae8438c6679ebc50056de275a900c
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-24 11:42:12 +0100

    Add file

---
+++ AS_2024_2025/5IA_Informatica/Lezioni/Esercizi_ModellazioneConcettuale/Esercizio06/query.sql
@@ -0,0 +1,28 @@
+-- Es1 : Visualizzare il nome e il cognome degli studenti che hanno preso in prestito un libro
+
+
+-- Es2 : Trovare il numero totale di libri presi in prestito da ciascuno studente
+
+
+-- Es3 : Visualizzare il titolo e l'autore dei libri presi in prestito da uno studente con un determinato cognome
+
+
+-- Es4 : Trovare la media dei giorni di prestito dei libri
+
+
+-- Es5 : Visualizzare il nome e il cognome dei bibliotecari che hanno gestito prestiti di libri
+
+
+-- Es6 : Trovare il numero di prestiti gestiti da ciascun bibliotecario
+
+
+-- Es7 : Visualizzare il titolo dei libri che non sono mai stati presi in prestito
+
+
+-- Es8 : Trovare il numero totale di libri presi in prestito per ciascun genere
+
+
+-- Es9 : Visualizzare il nome e il cognome degli studenti che hanno preso in prestito più di 3 libri
+
+
+-- Es10 : Trovare il titolo e l'autore del libro più vecchio presente nel database
