9e76beecd4a37a8dbdcf3e83813e983af684f317
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-25 10:51:36 +0100

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_01.c
@@ -0,0 +1,26 @@
+/*
+Iterazione 1 (Utilizzo del while)
+Creare un programma in C che prenda in input da parte dell'utente un valore intero (n).
+Il programma deve eseguire la somma di tutti i valori che vanno da 1 ad n, dove n è il valore inserito dall'utente.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, somma = 0;
+    int i = 1; // Inizializzazione della variabile di controllo del ciclo
+    
+    // Input del numero
+    printf("Inserisci un numero: ");
+    scanf("%d", &n);
+
+    // Calcolo della somma   
+    while (i <= n) { // Condizione del ciclo: continua finché i è minore o uguale a n
+        somma = somma + i; // Aggiunge il valore corrente di i alla somma
+        i++; // Incremento della variabile di controllo del ciclo
+    }
+    // Quando la condizione del ciclo non è più vera, il ciclo termina
+
+    // Stampa del risultato
+    printf("La somma dei valori da 1 a %d è: %d\n", n, somma);
+}
---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_02.c
@@ -0,0 +1,27 @@
+/*
+Iterazione 2 (Utilizzo del while)
+Creare un programma in C che prenda in input da parte dell'utente un valore intero (n).
+Il programma deve eseguire il fattoriale di n.
+N.B. Fattoriale di n = n * (n-1) * (n-2) * ... * 1
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, fattoriale = 1;
+    
+    // Input del numero
+    printf("Inserisci un numero: ");
+    scanf("%d", &n);
+
+    // Calcolo del fattoriale
+    int i = 1; // Inizializzazione della variabile di controllo del ciclo
+    while (i <= n) { // Condizione del ciclo: continua finché i è minore o uguale a n
+        fattoriale *= i; // Moltiplica il valore corrente di i al fattoriale
+        i++; // Incremento della variabile di controllo del ciclo
+    }
+    // Quando la condizione del ciclo non è più vera, il ciclo termina
+
+    // Stampa del risultato
+    printf("Il fattoriale di %d è: %d\n", n, fattoriale);
+}
