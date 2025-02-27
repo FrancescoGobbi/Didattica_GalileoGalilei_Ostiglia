07bb5ca5552f6b6491410d19d1fd55ce61335927
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-02-27 11:05:58 +0100

    Add file C

--- AS_2024_2025/3TrED_Informatica/UD_4_Array/Array_4.c
+++ AS_2024_2025/3TrED_Informatica/UD_4_Array/Array_4.c
@@ -6,6 +6,27 @@ L'array deve essere riempito con dei valori interi inseriti dall'utente.
 Il programma deve stampare al contrario i valori inseriti.
 */
 
+#define dim 10
+
 int main() {
-    // TO DO...
+    int vet[10] = {0};
+    int i; // Per scorrere il vettore
+
+    // Prendiamo in input i valori per il vettore
+    for(i = 0; i < dim; i++) {
+        printf("Inserisci un elemento in posizone [%d]: ", i);
+        scanf("%d", &vet[i]);
+    }
+
+    // Stampiamo il vettore inziale
+    for(i = 0; i < dim; i++) {
+        printf("%3d", vet[i]);
+    }
+    printf("\n");
+
+    // Stampa al contrario i valori inseriti
+    for(i = dim - 1; i >= 0 ; i-- ) {
+        printf("%3d", vet[i]);
+    }
+    printf("\n");
 }
