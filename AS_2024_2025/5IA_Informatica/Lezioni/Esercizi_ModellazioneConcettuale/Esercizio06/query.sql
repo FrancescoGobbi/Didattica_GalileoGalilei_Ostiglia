46dba85477634121b5b1f95091304f7fbf5457a1
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-06 13:00:48 +0100

    Add file

--- AS_2024_2025/5IA_Informatica/Lezioni/Esercizi_ModellazioneConcettuale/Esercizio06/query.sql
+++ AS_2024_2025/5IA_Informatica/Lezioni/Esercizi_ModellazioneConcettuale/Esercizio06/query.sql
@@ -1,13 +1,37 @@
--- Es1 : Visualizzare il nome e il cognome degli studenti che hanno preso in prestito un libro
+-- Es1 : Visualizzare il nome e il cognome degli studenti che 
+-- hanno preso in prestito un libro
+SELECT S.Nome, S.Cognome
+FROM Prestito AS P, Studente AS S
+WHERE S.Matricola = P.ID_Studente
+GROUP BY S.Matricola
 
 
 -- Es2 : Trovare il numero totale di libri presi in prestito da ciascuno studente
+SELECT S.Nome, COUNT(S.Matricola) AS NumeroPrestiti
+FROM Prestito AS P, Studente AS S
+WHERE S.Matricola = P.ID_Studente 
+GROUP BY S.Matricola
 
 
--- Es3 : Visualizzare il titolo e l'autore dei libri presi in prestito da uno studente con un determinato cognome
+-- Es3 : Visualizzare il titolo e l'autore dei libri presi 
+-- in prestito da uno studente con un determinato cognome ('Rossi')
+SELECT L.Titolo, L.Autore
+FROM Libro AS L, Studente AS S, Prestito AS P
+WHERE L.ISBN = P.ID_Libro AND S.Matricola = P.ID_Studente AND S.Cognome = 'Rossi'
 
 
 -- Es4 : Trovare la media dei giorni di prestito dei libri
+SELECT L.Titolo, 
+FROM Libro AS L, Prestito AS P
+WHERE L.ISBN = P.ID_Libro
+
+
+
+
+
+
+
+
 
 
 -- Es5 : Visualizzare il nome e il cognome dei bibliotecari che hanno gestito prestiti di libri
