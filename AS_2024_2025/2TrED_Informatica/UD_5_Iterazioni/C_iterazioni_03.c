cd3f7cf707bd818a06e74261689d431b08598350
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-10 09:51:13 +0100

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_iterazioni_03.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_iterazioni_03.c
@@ -4,6 +4,8 @@ Creare un programma in C che prenda in input da parte dell'utente un valore inte
 Il programma deve stampare tutti i numeri pari compresi tra 1 ed n, estremi compresi.
 */
 
+#include <stdio.h>
+
 int main() {
     // TO DO...
 }
