07b7a8f95ed8e4d32340cd7006a57bd339cb8f4a
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-11 00:17:34 +0100

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_05.c
@@ -0,0 +1,15 @@
+/*
+Iterazione 5 (Utilizzo del while)
+Creare un programma in C che prenda in input da parte dell'utente un valore intero (n).
+Il programma deve determinare se il valore inserito è un numero perfetto. 
+Se il valore è un numero perfetto, il programma deve stampare "Il numero è perfetto."
+Se il valore non è un numero perfetto, il programma deve stampare "Il numero non è perfetto."
+N.B. Un numero perfetto è un numero intero positivo che è uguale alla somma dei suoi divisori propri (escluso il numero stesso).
+Esempio: 6 è un numero perfetto perché 6 = 1 + 2 + 3.
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
