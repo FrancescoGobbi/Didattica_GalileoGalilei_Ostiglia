09feb9d2097ce5c2625ca62d77cf85cfb264c682
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-10 09:48:10 +0100

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_iterazioni_03.c
@@ -0,0 +1,9 @@
+/*
+Iterazione 3 (Utilizzo del while)
+Creare un programma in C che prenda in input da parte dell'utente un valore intero (n).
+Il programma deve stampare tutti i numeri pari compresi tra 1 ed n, estremi compresi.
+*/
+
+int main() {
+    // TO DO...
+}
