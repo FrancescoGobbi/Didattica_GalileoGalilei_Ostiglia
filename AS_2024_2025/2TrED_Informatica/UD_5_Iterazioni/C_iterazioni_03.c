b1ce6a5e6794bd350e7b2351b2082b6450211aa8
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-10 09:56:33 +0100

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_iterazioni_03.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_iterazioni_03.c
@@ -7,5 +7,21 @@ Il programma deve stampare tutti i numeri pari compresi tra 1 ed n, estremi comp
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    int n;
+    int i = 1; // Inizializzazione della variabile di controllo del ciclo
+    // Alternativa: int n, i = 1;
+    
+    // Input del numero
+    printf("Inserisci un numero: ");
+    scanf("%d", &n);
+
+    // Stampa i valori da 1 a N 
+    while (i <= n) { // Condizione del ciclo: continua finché i è minore o uguale a n
+        if (i % 2 == 0 ){
+            printf("%d ", i); // Stampa il valore corrente di i
+        }
+        i = i + 1; // Incremento della variabile di controllo del ciclo (Alternativa: i++;)
+    }
+    // Quando la condizione del ciclo non è più vera, il ciclo termina
+    printf("\n"); // Per andare a capo a fine programma (Serve principalmente per MacOS, non per DevC++)
 }
