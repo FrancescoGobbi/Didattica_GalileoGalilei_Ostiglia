0de7dc7bb729c001a27f6ac5f9c33658c34fb1b1
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-18 17:56:35 +0100

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_05.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_05.c
@@ -11,5 +11,29 @@ Esempio: 6 è un numero perfetto perché 6 = 1 + 2 + 3.
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    // Creazione delle variaibli
+    int n;
+    int i = 1; // Inizializzazione della variabile di controllo del ciclo
+    int sum = 0;
+    // Alternativa: int n, i = 1, sum = 0;
+    
+    // Input del numero
+    printf("Inserisci un numero: ");
+    scanf("%d", &n);
+
+    // Controllo i valori da 1 a N 
+    while (i < n) { // Condizione del ciclo: continua finché i è minore di n
+        if (n % i == 0){
+            sum += i; // Alternativa: sum = sum + i;
+        }
+        i = i + 1; // Incremento della variabile di controllo del ciclo (Alternativa: i++;)
+    }
+
+    // Controllo se il numero è perfetto
+    if (sum == n) {
+        printf("Il numero è perfetto.\n");
+    }
+    else {
+        printf("Il numero NON è perfetto.\n");
+    }
 }
--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_06.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_06.c
@@ -11,5 +11,24 @@ Se A = 2 e B = 10, il programma deve stampare "Ci sono 5 numeri pari tra 2 e 10.
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    // Creazione delle variaibli
+    int A, B;
+    int i = 0; // Inizializzazione della variabile di controllo del ciclo
+    int count = 0;
+    // Alternativa: int A, B, i = 0, count = 0;
+    
+    // Input del numero
+    printf("Inserisci due numeri: ");
+    scanf("%d %d", &A, &B);
+
+    // Controllo i valori da A a B 
+    while (A <= B) { // Condizione del ciclo: continua finché A è minore o uguale a B
+        if (A % 2 == 0){
+            count++; // Alternativa: count = count + 1;
+        }
+        A = A + 1; // Incremento della variabile di controllo del ciclo (Alternativa: A++;)
+    }
+
+    // Stampa finale
+    printf("Ci sono %d numeri pari tra %d e %d.\n", count, A, B);
 }
