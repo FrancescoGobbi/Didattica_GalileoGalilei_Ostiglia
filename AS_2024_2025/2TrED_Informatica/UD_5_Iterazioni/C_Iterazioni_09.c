0a54d38c02fdae9dd723565001899b31c8d77339
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-18 10:51:56 +0100

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_09.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_09.c
@@ -5,7 +5,7 @@ Il programma vede sommare tutti i numeri compresi tra A e B, estremi inclusi, ch
 Il programma deve stampare la somma di tutti i numeri divisibili per 5 compresi tra A e B.
 
 Esempio:  
-Se A = 1 e B = 10, il programma deve stampare "La somma dei numeri divisibili per 5 tra 1 e 10 è: 5."
+Se A = 1 e B = 10, il programma deve stampare "La somma dei numeri divisibili per 5 tra 1 e 10 è: 15."
 */
 
 #include <stdio.h>
