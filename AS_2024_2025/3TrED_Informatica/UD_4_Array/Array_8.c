a98d03b503f8e8bc6c7215b2717517c76c6697b2
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-24 13:50:18 +0100

    Add file C

---
+++ AS_2024_2025/3TrED_Informatica/UD_4_Array/Array_8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+Creare il codice in C di un programma che crea un array di interi di dimensione 10.
+Il programma deve riempire l'array con numeri casuali compresi tra 1 e 100.
+Il programma deve poi eseguire la sommatoria di tutti i numeri presenti nel vettore
+e stampare il risultato.
+*/
+
+#define DIM 10
+
+int main() {
+    // TO DO...
+}
