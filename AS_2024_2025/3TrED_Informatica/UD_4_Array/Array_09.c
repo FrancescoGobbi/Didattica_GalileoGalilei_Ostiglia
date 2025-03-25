81e4d73135f7dcb2ddc24e16bb0bbee12319ac70
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-25 09:53:12 +0100

    Add file C

--- AS_2024_2025/3TrED_Informatica/UD_4_Array/Array_09.c
+++ AS_2024_2025/3TrED_Informatica/UD_4_Array/Array_09.c
@@ -14,7 +14,40 @@ Il programma deve poi stampare tale valore.
 void initRandomVet(int vet[], int dim);
 
 int main() {
-    // TO DO...
+    int vet[DIM];
+    int i; // Variabile di iterazione
+    int max, max2; // Variabili per il massimo 
+
+    // Chiamata della funzione per generare dei numeri casuali nel vettore
+    initRandomVet(vet, DIM);
+
+    // Vettore iniziale
+    printf("Il vettore iniziale è: \n");
+    for(i = 0; i< DIM; i++) {
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
+
+    // Troviamo il massimo in assoluto del vettore
+    max = vet[0];
+
+    // Trovare il valore massimo assoluto nell'array
+    for (i = 1; i < DIM; i++) {
+        if (vet[i] > max) { // Per trovare il massimo
+            max = vet[i];
+        }
+    }
+
+    // Troviamo il secondo massimo del vettore
+    max2 = vet[0];
+
+    for (i = 1; i < DIM; i++) {
+        if (vet[i] < max && vet[i] > max2) {
+            max2 = vet[i];
+        }
+    }
+
+    printf("Il secondo massimo del vettore è: %d\n", max2);
 }
 
 void initRandomVet(int vet[], int dim) {
@@ -24,6 +57,6 @@ void initRandomVet(int vet[], int dim) {
     srand(time(NULL));
 
     for(i = 0; i< dim; i++) {
-        vet[i] = rand() %10;
+        vet[i] = rand() %100 + 1;
     }
 }
