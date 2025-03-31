7687ee2b919d690450b4eac5566b4857d3aa04c0
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-31 09:52:40 +0200

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_12.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_12.c
@@ -7,5 +7,22 @@ Il programma deve eseguire la somma di tutti i valori che vanno da 1 ad n, dove 
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    int n, somma = 0;
+    
+    // Input del numero
+    printf("Inserisci un numero: ");
+    scanf("%d", &n);
+
+    // Calcolo della somma   
+    // Utilizzo del for: il for ha bisogno di 3 componenti (separati dal ;)
+    // Prima componente: Inizializzazione della variabile di controllo del ciclo
+    // Seconda componente: Condizione del ciclo: continua finché i è minore o uguale a n
+    // Terza componente: Incremento della variabile di controllo del ciclo
+    for (int i = 1; i <= n; i++) { // Condizione del ciclo: continua finché i è minore o uguale a n
+        somma = somma + i; // Aggiunge il valore corrente di i alla somma
+    }
+    // Quando la condizione del ciclo non è più vera, il ciclo termina
+
+    // Stampa del risultato
+    printf("La somma dei valori da 1 a %d è: %d\n", n, somma);
 }
