c0c1ea4b6fdaa1ef4349b7d34169be4761d9aeb8
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-31 14:16:24 +0200

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_15.c
@@ -0,0 +1,14 @@
+/*
+Iterazione 15 (Utilizzo del for)
+Creare un programma in C che prenda in input da parte dell'utente un valore intero (n).
+Il programma deve determinare se il valore inserito è un numero primo.
+Se il valore è un numero primo, il programma deve stampare "Il numero è primo."
+Se il valore non è un numero primo, il programma deve stampare "Il numero non è primo."
+N.B. Un numero primo è un numero maggiore di 1 che è divisibile solo per 1 e per se stesso.
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
