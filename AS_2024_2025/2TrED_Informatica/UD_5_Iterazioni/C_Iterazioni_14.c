412ac945b19c451f38f9e52ed23d2ed30bf4f6fc
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-03-31 09:14:57 +0200

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_14.c
@@ -0,0 +1,17 @@
+/*
+Iterazione 1 (Utilizzo del for)
+Creare un programma in C che prenda in input da parte dell'utente un valore intero (n).
+Il programma deve stampare tutti i numeri pari tra 1 ed n (incluso n se è pari).
+Se n è minore di 1, il programma deve stampare "Nessun numero da stampare."
+
+Esempio:
+Se n = 10, il programma deve stampare "2 4 6 8 10"
+Se n = 5, il programma deve stampare "2 4"
+Se n = 0, il programma deve stampare "Nessun numero da stampare."
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
