c8d0004657db2100ebc3240b11d58f5976090da7
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-04-07 12:38:25 +0200

    Add file C

---
+++ AS_2024_2025/3TrED_Informatica/UD_4_Array/Funzioni_Array_5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define dimensione 10
+/*
+Crea una funzione che prende in input un vettore e la sua dimensione.
+La funzione trovare e stampare il secondo massimo ed il secondo minimo del vettore.
+*/
+
+void SecondoMaxMinVet(int vet[], int dim);
+
+int main() {
+    // TO DO...
+}   
+
+void SecondoMaxMinVet(int vet[], int dim){
+    // TO DO...
+}
