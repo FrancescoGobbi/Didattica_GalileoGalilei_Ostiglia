6b8ea8995147c0101ca282feb6966f7e9164c0f3
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-04-07 09:54:50 +0200

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_13.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_13.c
@@ -8,5 +8,23 @@ Il programma deve poi stampre tale valore del fattoriale.
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    int n, fattoriale = 1;
+    int i = 1; // Creo la variabile di iterazione
+    
+    // Input del numero
+    printf("Inserisci un numero: ");
+    scanf("%d", &n);
+
+    // Calcolo del fattoriale   
+    // Utilizzo del for: il for ha bisogno di 3 componenti (separati dal ;)
+    // Prima componente: Inizializzazione della variabile di controllo del ciclo
+    // Seconda componente: Condizione del ciclo: continua finché i è minore o uguale a n
+    // Terza componente: Incremento della variabile di controllo del ciclo
+    for (i = 1; i <= n; i++) { // Condizione del ciclo: continua finché i è minore o uguale a n
+        fattoriale = fattoriale * i; // Aggiunge il valore corrente di i alla somma
+    }
+    // Quando la condizione del ciclo non è più vera, il ciclo termina
+
+    // Stampa del risultato
+    printf("Il fattoriale della variabile %d è: %d\n", n, fattoriale);
 }
