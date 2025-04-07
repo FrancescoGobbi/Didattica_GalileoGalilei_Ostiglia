21329a98fc5a2718b2cd1cd2d55dd97ff78b7dc5
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-04-07 12:57:12 +0200

    Add file C

--- AS_2024_2025/3TrED_Informatica/UD_4_Array/Funzioni_Array_4.c
+++ AS_2024_2025/3TrED_Informatica/UD_4_Array/Funzioni_Array_4.c
@@ -9,10 +9,34 @@ ESEMPIO:
 Se vet[0] = 5, vet[1] = 2, vet[2] = 3, 
 allora vet[0] = 2, vet[1] = 3, vet[2] = 5
 */
+
+/**
+ * @brief Funzione che inizializza un vettore di interi con valori randomici.
+ * 
+ * @param vet 
+ * @param dim 
+ */
+void initRandomVet(int vet[], int dim);
+
+/**
+ * @brief Funzione che ordina un vettore con algoritmo Bubble Sort
+ * 
+ * @param vet 
+ * @param dim 
+ */
 void BubbleSort(int vet[], int dim);
 
 int main() {
     // TO DO...
-}   
+}  
+
+void initRandomVet(int vet[], int dim) {
+    int i;
+
+    for(i = 0; i< dim; i++) {
+        vet[i] = rand() %100 + 1;// Da eventualmente cambiare
+        // in base alla richiesta del problema
+    }
+}
 
 void BubbleSort(int vet[], int dim);
--- AS_2024_2025/3TrED_Informatica/UD_4_Array/Funzioni_Array_5.c
+++ AS_2024_2025/3TrED_Informatica/UD_4_Array/Funzioni_Array_5.c
@@ -6,11 +6,34 @@ Crea una funzione che prende in input un vettore e la sua dimensione.
 La funzione trovare e stampare il secondo massimo ed il secondo minimo del vettore.
 */
 
+/**
+ * @brief Funzione che inizializza un vettore di interi con valori randomici.
+ * 
+ * @param vet 
+ * @param dim 
+ */
+void initRandomVet(int vet[], int dim);
+
+/**
+ * @brief Funzione che ordina un vettore con algoritmo Bubble Sort
+ * 
+ * @param vet 
+ * @param dim 
+ */
 void SecondoMaxMinVet(int vet[], int dim);
 
 int main() {
     // TO DO...
 }   
+
+void initRandomVet(int vet[], int dim) {
+    int i;
+
+    for(i = 0; i< dim; i++) {
+        vet[i] = rand() %100 + 1;// Da eventualmente cambiare
+        // in base alla richiesta del problema
+    }
+}
 
 void SecondoMaxMinVet(int vet[], int dim){
     // TO DO...
