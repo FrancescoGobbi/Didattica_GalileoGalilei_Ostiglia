13a045b17599418ff5448beee74e680138484714
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-04-14 08:55:52 +0200

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_18.c
@@ -0,0 +1,16 @@
+/*
+Iterazione 18 (Utilizzo del do-while)
+Creare un programma in C che prenda in input da parte dell'utente due valori interi (a e b).
+IL programma deve far in modo che il valore di A sia maggiore di 0 e minore di 100.
+Mentre il valore di B deve essere maggiore di A.
+
+Il programma deve eseguire la differenza tra i due numeri (b-a) e stampare il risultato.
+Il programma deve continuare a chiedere all'utente di inserire un numero valido finché non vengono rispettate le condizioni.
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
