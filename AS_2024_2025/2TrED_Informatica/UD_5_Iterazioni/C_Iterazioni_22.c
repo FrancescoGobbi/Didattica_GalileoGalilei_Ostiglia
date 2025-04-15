3ec88cd736d3a55e7d1d742294c9a7ca62a0708c
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-04-15 11:44:23 +0200

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_22.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_22.c
@@ -1,38 +1,17 @@
 /*
 Iterazione 22 (Utilizzo del do-while)
 Creare un programma in C che prenda in input da parte dell'utente due valori interi A e B.
+Il valore di A deve essere compreso tra 1 e 10, mentre il valore di B deve essere compreso tra 1 e 10.
+Il programma deve eseguire e stampare la potenza A^B (A elevato alla B) utilizzando l'iterezione per la potenza.
+Quindi dovrò chiedere in input A e B, e poi calcolare la potenza A^B.
 
+ESEMPIO:
+Se l'utente inserisce A=2 e B=3, il programma deve stampare "2^3 = 8".
+Se l'utente inserisce A=3 e B=2, il programma deve stampare "3^2 = 9".
 */
 
 #include <stdio.h>
 
 int main() {
-    int A, B;
-    int count = 0;
-    int i; // Variabile di iterazione
-    int val; // Variabile per prendere e salvvare i nuovi numeri in input
-
-    do{
-        printf("Insersci un valore per A: ");
-        scanf("%d", &A);
-    }while(A<=1 || A>=10); // Noi VOGLIAMO (A>1 && A<10)
-
-    do{
-        printf("Insersci un valore per B: ");
-        scanf("%d", &B);
-    }while(B<=A); // Noi VOGLIAMO (B>A)
-
-    // Esecuzione con ciclo for per chiedere in input A-valori interi all'untente
-    for(i = 1; i <= A; i++) {
-        printf("Inserisci un numero intero: ");
-        scanf("%d", &val); // Scriverò sempre i valori nella stessa variabile, quindi si sovrascrivono
-
-        // Controllo se il numero è maggiore di B
-        if (val > B) {
-            count++; // Incremento il contatore
-        }
-    }
-
-    // Stampa del risultato finale
-    printf("Ho trovato %d numeri in input che sono maggiori di B, ovvero di %d.\n", count, B);
+    // TO DO...
 }
---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_23.c
@@ -0,0 +1,26 @@
+/*
+Iterazione 23 (Utilizzo del do-while)
+Creare un programma in C che prenda in input 3 numeri (A, B e C).
+Questi numeri devono essere presi in input in ordine crescente, quindi A < B < C.
+Il programma stampre verificare se i numeri inseriti sono tutti numeri perfetti.
+Se sono tutti numeri perfetti, il programma deve stampare "Tutti i numeri sono perfetti."
+Se non sono tutti numeri perfetti, il programma deve stampare "Non tutti i numeri sono perfetti."
+
+Un numero perfetto è un numero intero positivo che è uguale alla somma dei suoi divisori propri 
+positivi (escluso se stesso).
+
+ESEMPIO: 
+Se l'utente inserisce 6, 28 e 496, il programma deve stampare "Tutti i numeri sono perfetti."
+Se l'utente inserisce 6, 28 e 12, il programma deve stampare "Non tutti i numeri sono perfetti."
+Se l'utente inserisce 12, 6 e 28, il programma deve stampare "Non tutti i numeri sono perfetti."
+Se l'utente inserisce 6, 28 e 496, il programma deve stampare "Tutti i numeri sono perfetti."
+
+6 è un numero perfetto, in quanto i suoi divisori sono 1, 2 e 3, e 1+2+3=6.
+28 è un numero perfetto, in quanto i suoi divisori sono 1, 2, 4, 7 e 14, e 1+2+4+7+14=28.
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
