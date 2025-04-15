c19ba4191385d3f9ecfbb6917e9d709ba1e4e1b9
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-04-15 11:12:08 +0200

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_19.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_19.c
@@ -14,27 +14,29 @@ int main() {
     int A, B;
     int count = 0;
     int i; // Variabile di iterazione
-    int val; // Varaibile per prendere i nuovi numeri in input
+    int val; // Variabile per prendere e salvvare i nuovi numeri in input
 
     do{
         printf("Insersci un valore per A: ");
         scanf("%d", &A);
-    }while(A<=1 || A>=10); // Noi VOGLIAMO A>1 && A<10
+    }while(A<=1 || A>=10); // Noi VOGLIAMO (A>1 && A<10)
 
     do{
         printf("Insersci un valore per B: ");
         scanf("%d", &B);
-    }while(B<=A); // Noi VOGLIAMO B>A
+    }while(B<=A); // Noi VOGLIAMO (B>A)
 
-    // Esecuzione con ciclo for
+    // Esecuzione con ciclo for per chiedere in input A-valori interi all'untente
     for(i = 1; i <= A; i++) {
         printf("Inserisci un numero intero: ");
-        scanf("%d", &val);
+        scanf("%d", &val); // Scriverò sempre i valori nella stessa variabile, quindi si sovrascrivono
 
+        // Controllo se il numero è maggiore di B
         if (val > B) {
-            count++;
+            count++; // Incremento il contatore
         }
     }
 
+    // Stampa del risultato finale
     printf("Ho trovato %d numeri in input che sono maggiori di B, ovvero di %d.\n", count, B);
 }
