e6c1caf28e844ca04501b3578c5637cf85e1a4fe
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-05-05 09:24:09 +0200

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_6_DoppieIterazioni/C_DoppieIterazioni_00.c
+++ AS_2024_2025/2TrED_Informatica/UD_6_DoppieIterazioni/C_DoppieIterazioni_00.c
@@ -22,19 +22,19 @@ int main() {
     
     printf("\n-------------------\n");
 
-    // Iterazione per le righe della tabella
+    // Iterazione per le righe della tabella (ciclo for più esterno)
     for (i = 1; i <= 10; i++) {
 
         // Stampa per intestazione della riga
         printf("%2d |", i); // Stampa il numero della riga
 
-        // Iterazione per le colonne della tabella
+        // Iterazione per le colonne della tabella (ciclo for più interno)
         for (j = 1; j <= 10; j++) {
             risultato = i * j; // Calcolo del prodotto
             printf("%2d ", risultato); // Stampa del risultato
             // %2d per allineare i numeri a destra in una colonna di 2 caratteri
-        }
+        } // Fine del ciclo interno
         // Vado a capo riga
         printf("\n"); // Nuova riga dopo ogni riga della tabella
-    }
+    } // Fine del ciclo esterno
 }
