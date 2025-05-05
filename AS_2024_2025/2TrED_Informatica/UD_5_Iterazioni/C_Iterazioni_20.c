4be8a99c5cfa7af6c75159721ee839a3ca3bf20d
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-05-05 09:50:39 +0200

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_20.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_20.c
@@ -11,5 +11,32 @@ tra tutti questi.
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    int a;
+    float media = 0;
+    int somma = 0;
+    int num;
+    int i;
+
+    // Prendo in input iul valore di a corretto (compereso tra 1 e 20)
+    do{ 
+        printf("Inserisci il valore di a: ");
+        scanf("%d", &a);
+    }while(a<1 || a>20);
+    // NOI VOGLIAMO: a>=1 && a<=20
+    // Quindi, quello che NON VOGLIAMO: a<1 || a>20
+
+    // Prendo in input A-valori e li sommo tra loro
+    // while(i<=a)
+    for (i=1 ; i<=a; i++) {
+        do{ // Prendo in inpit solo numeri positivi da parte dell'utente
+            printf("Inserisci il %d^ numero: ", i);
+            scanf("%d", &num);
+        }while(a<=0);
+        
+        somma = somma + num; // somma += num;
+    }
+
+    // Eseguo la media
+    media = (float)somma / a; 
+    printf("La media finale è: %f\n", media);
 }
