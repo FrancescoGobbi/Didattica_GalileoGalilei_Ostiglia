3dd1dd32fe7dc4b457b457c79e0b10a24e0d9823
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-05-05 09:52:14 +0200

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_21.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_21.c
@@ -4,8 +4,8 @@ Creare un programma in C che prenda in input da parte dell'utente un valore inte
 Il valore deve essere compreso tra 1 e 20.
 
 Il programma deve eseguire e stampare la media sei soli valori pari tra gli A-valori inseriti dall'utente.
-Quindi dovrò chiedere in input A-valori, quindi tanti quanti il valore di A. Dovrò fare la media
-tra solamente quelli pari.
+Quindi dovrò chiedere in input altri A-valori, quindi tanti quanti il valore di A. 
+Dovrò fare la media tra solamente quei numeri pari inseriti.
 */
 
 #include <stdio.h>
