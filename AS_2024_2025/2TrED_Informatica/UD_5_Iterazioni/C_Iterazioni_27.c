88bfff8869f72bcbe0b19b7359c47d44a087aaaf
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-05-12 08:59:51 +0200

    Add file C

---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_27.c
@@ -0,0 +1,16 @@
+/*
+Iterazione 26 (Utilizzo delle iterazioni)
+Creare un programma in C che prenda in input da parte dell'utente due valori A e B,
+in cui A > B > 10.
+Il programma deve sommare e stampare tutti i numeri primi compresi tra A e B.
+N.B. Devo stare attento a non sommare i numeri compresi tra A e B. Quindi A e B che sarà un range
+deve essere letto nel modo corretto. Il range solitamente parte da un numero più piccolo e va a un numero più grande.
+
+Successivamente il programma deve sottrarre tutti i numeri dispari compresi tra A e B.
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
---
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_28.c
@@ -0,0 +1,24 @@
+/*
+Iterazione 26 (Utilizzo delle iterazioni)
+Creare un programma in C che prenda in input da parte dell'utente tre valori A, B e C.
+I valori devono avere le seguenti condizioni:
+- A > 0
+- B > A
+- C > B
+
+Il programma deve verificare se i numeri inseriti sono tutti numeri perfetti.
+Se sono tutti numeri perfetti, il programma deve stampare "Tutti i numeri sono perfetti."
+Se non sono tutti numeri perfetti, il programma deve stampare "Non tutti i numeri sono perfetti."
+
+Definizione: Un numero perfetto è un numero intero positivo che è uguale alla somma dei suoi divisori propri,
+escluso se stesso.
+
+ESEMPIO:
+Se l'utente inserisce 6, 28 e 496, il programma deve stampare "Tutti i numeri sono perfetti."
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
