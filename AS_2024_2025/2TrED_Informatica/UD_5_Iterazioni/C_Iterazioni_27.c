cabce9a54b27941f837b248bdec2dfafb64b1312
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-05-12 10:27:19 +0200

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_27.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_27.c
@@ -12,5 +12,42 @@ Successivamente il programma deve sottrarre tutti i numeri dispari compresi tra 
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    int A, B; // Variabili per i numeri
+    int somma = 0; // Variabile per la somma dei numeri primi
+    int i, j; // Variabili di iterazione
+    int count; // Contatore per verificare se un numero è primo
+
+    // Prendo in input i valori di A e B
+    do{
+        
+    }while(A<= B || B <= 10); // Condizione per l'input corretto
+    // NOI VOGLIAMO: A > B && B > 10
+    // Quindi, quello che NON VOGLIAMO: A <= B || B <= 10
+
+    // Sommo i numeri primi compresi tra A e B
+    // Il numero A è più grande del numero B, quindi devo partire da B e andare fino ad A
+    // In questo caso è facile capire da quale variabile partire, in quanto A è più grande di B
+    // e questo è già definito dal testo
+    for (i = B; i <= A; i++) {
+        count = 0; // Resetto il contatore dei divisori
+        for (j = 1; j <= i; j++) { // Per verificare se il numero i numero è primo
+            if (i % j == 0) {
+                count++;
+            }
+        }
+        if (count == 2) { // Se il numero è primo
+            somma = somma + i; // Alternativa : somma += i;
+        }
+    }
+
+    // Sottraggo i numeri dispari compresi tra A e B
+    // Il numero A è più grande del numero B, quindi devo partire da B e andare fino ad A
+    for (i = B; i <= A; i++) {
+        if (i % 2 != 0) { // Se il numero è dispari
+            somma = somma - i; // Alternativa : somma -= i;
+        }
+    }
+
+    // Stampa dei risultati finali
+    printf("La somma dopo tutte le operazioni è: %d\n", somma);
 }
