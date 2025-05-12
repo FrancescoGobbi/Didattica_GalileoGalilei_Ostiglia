cff4881d003e3b637465af73e6ce4b1eeaf60286
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-05-12 09:59:11 +0200

    Add file C

--- AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_26.c
+++ AS_2024_2025/2TrED_Informatica/UD_5_Iterazioni/C_Iterazioni_26.c
@@ -16,9 +16,33 @@ Se l'utente inserisce A=1, il programma deve stampare "Non ci sono numeri primi 
 int main() {
     // Creaiamo le variabili utilizzate nel codice
     int A;
+    int somma = 0; // La somma dei numeri primi
+    int i, j; // Variabili di itarezione
+    int count = 0; // Verifico se un numero è primo
 
     // Richiedo in input il numero A, rispetto ai vincoli del testo
     do{
+        printf("Insesci un numero A compreso tra 1 e 20: ");
         scanf("%d", &A);
-    }while();
+    }while(A<1 || A>20);
+    // NOI VOGLIAMO: A>=1 && A<=20
+    // !(A>=1 && A<=20)
+    // Quindi NON VOGLIAMO: A<1 || A>20, che è il negato/duale della condizione sopra
+
+    // Andiamo avanti con il programma
+    for(i = 1; i<=A; i++){ // Guardo tutti i numeri primi compresi tra 1 e A
+        count = 0; // Azzero il contatore dei divisori
+        for(j=1 ; j<=i ; j++){ // Guardo tutti i numeri tra 1 ed i, estremi compresi
+            if (i % j == 0){ // Controllo se la j è un divisore di i
+                count++;
+            }
+        }
+    
+        if (count == 2) { // Se il numero è primo
+            somma = somma + i; // Sommo il numero primo trovato
+            printf("%d ", i); // Stampo il numero primo provato
+        }
+    } // Ho sommato tutti i numeri primi compresi tra 1 ed A
+
+    printf("\nLa somma dei numeri primi compresi tra %d e %d è: %d\n", 1, A, somma);
 }
