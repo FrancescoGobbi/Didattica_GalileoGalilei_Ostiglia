6516b0f7c582b5264c1ab0a89ce0965a359f35c5
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-09-25 08:07:15 +0200

    Add file C

---
+++ AS_2025_2026/3TrED_Informatica/UD_0_DoppieIterazioni/C_DoppieIterazioni_07.c
@@ -0,0 +1,32 @@
+/* Doppie Iterazioni 5 (Iterazioni annidate)
+
+Creare un programma in C che prenda in input da parte dell'utente un valore intero A ed un valore B.
+Il valore di A deve essere maggiore di 3 e B deve essere maggiore di A.
+
+Il programma deve stampare una figura con il medesimo pattern:
+* + * + *
++ * + * +
+* + * + *
++ * + * +
+
+Con A il numero di righe e B il numero di colonne.
+
+ESEMPIO:
+Se l'utente inserisce A=3 e B=5, il programma deve stampare:
+* + * + *
++ * + * +
+* + * + *
+
+Se l'utente inserisce A=4 e B=6, il programma deve stampare:
+* + * + * +
++ * + * + +
+* + * + * +
++ * + * + +
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
