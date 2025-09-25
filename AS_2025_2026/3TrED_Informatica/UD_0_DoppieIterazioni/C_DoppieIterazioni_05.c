df097553b851ad47263c589059d1dbcc1c903a84
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-09-25 08:04:11 +0200

    Add file C

---
+++ AS_2025_2026/3TrED_Informatica/UD_0_DoppieIterazioni/C_DoppieIterazioni_05.c
@@ -0,0 +1,27 @@
+/* Doppie Iterazioni 5 (Iterazioni annidate)
+
+Creare un programma in C che prenda in input da parte dell'utente un valore intero A.
+Il valore A deve essere strettamente maggiore di 1.
+
+Il programma deve stampare il triangolo di Floid.
+
+ESEMPIO:
+Se l'utente inserisce A=3, il programma deve stampare:
+1
+2 3
+4 5 6
+
+Se l'utente inserisce A=5, il programma deve stampare:
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
