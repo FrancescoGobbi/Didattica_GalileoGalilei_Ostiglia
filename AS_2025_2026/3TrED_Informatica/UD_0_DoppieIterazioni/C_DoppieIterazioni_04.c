12e45f4e1666d650a8fd9f552547e632983fc48a
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-09-29 00:14:27 +0200

    Add file C

--- AS_2025_2026/3TrED_Informatica/UD_0_DoppieIterazioni/C_DoppieIterazioni_04.c
+++ AS_2025_2026/3TrED_Informatica/UD_0_DoppieIterazioni/C_DoppieIterazioni_04.c
@@ -23,5 +23,31 @@ Se l'utente inserisce A=5 e B = 6, il programma deve stampare:
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    int A, B; // Variabili per le dimensioni della figura
+    int i, j; // Variabili di iterazione
+
+    // Prendo in input il valore di A compreso tra 1 e 10
+    do {
+        printf("Inserisci un valore per A (compreso tra 1 e 10): ");
+        scanf("%d", &A);
+    } while (A < 1 || A > 10); // NOI VOGLIAMO: A>=1 && A<=10
+
+    // Prendo in input il valore di B compreso tra 1 e 10
+    do {
+        printf("Inserisci un valore per B (compreso tra 1 e 10): ");
+        scanf("%d", &B);
+    } while (B < 1 || B > 10); // NOI VOGLIAMO: B>=1 && B<=10
+
+    // Stampa della figura formata da asterischi solo sul perimetro
+    for (i = 1; i <= A; i++) { // Ciclo esterno per le righe
+        for (j = 1; j <= B; j++) { // Ciclo interno per le colonne
+            if (i == 1 || i == A || j == 1 || j == B){
+                printf("* "); // Stampa dell'asterisco
+            }
+            else {
+                printf("  ");
+            }
+        }
+        printf("\n"); // Vado a capo dopo ogni riga
+    }
 }
