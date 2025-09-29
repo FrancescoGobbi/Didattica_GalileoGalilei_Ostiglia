9d8cf1bbecc4a397580bbdcbbd142f2d1deca8ff
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-09-29 08:13:40 +0200

    Add file C

---
+++ AS_2025_2026/3TrED_Informatica/UD_0_DoppieIterazioni/C_DoppieIterazioni_08.c
@@ -0,0 +1,28 @@
+/* Doppie Iterazioni 7 (Iterazioni annidate)
+
+Creare un programma in C che prenda in input da parte dell'utente un valore intero A.
+Il numero deve essere compreso tra 1 e 10.
+
+Il pramma deve eseguire e stampare una tabella con solo i valori pari in una tabella di dimensioni AxA.
+
+
+ESEMPIO:
+Se l'utente inserisce A=3, il programma deve stampare:
+2 4 6
+8 10 12
+14 16 18
+
+Se l'utente inserisce A=5, il programma deve stampare:
+2 4 6 8 10
+12 14 16 18 20
+22 24 26 28 30
+32 34 36 38 40
+42 44 46 48 50
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
