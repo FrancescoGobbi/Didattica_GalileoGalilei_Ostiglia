b1948c3b18f17e49f52e4c0d36e67cc69d8cbeb2
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-10-06 13:56:58 +0200

    Add file

--- AS_2025_2026/3TrED_Informatica/UD_0_DoppieIterazioni/C_DoppieIterazioni_08.c
+++ AS_2025_2026/3TrED_Informatica/UD_0_DoppieIterazioni/C_DoppieIterazioni_08.c
@@ -13,7 +13,7 @@ Se l'utente inserisce A=3, il programma deve stampare:
 14 16 18
 
 Se l'utente inserisce A=5, il programma deve stampare:
-2 4 6 8 10
+ 2  4  6  8 10
 12 14 16 18 20
 22 24 26 28 30
 32 34 36 38 40
@@ -24,5 +24,22 @@ Se l'utente inserisce A=5, il programma deve stampare:
 #include <stdio.h>
 
 int main() {
-    // TO DO...
+    int A; // Variabile per la dimensione della tabella
+    int i, j; // Variabili di iterazione
+    int numero_pari = 2; // Inizializzo il primo numero pari
+
+    // Prendo in input il valore di A compreso tra 1 e 10
+    do {
+        printf("Inserisci un valore per A (compreso tra 1 e 10): ");
+        scanf("%d", &A);
+    } while (A < 1 || A > 10); // NOI VOGLIAMO: 1 <= A <= 10
+
+    // Stampa della tabella con i numeri pari
+    for (i = 1; i <= A; i++) { // Ciclo esterno per le righe
+        for (j = 1; j <= A; j++) { // Ciclo interno per le colonne
+            printf("%2d ", numero_pari); // Stampa del numero pari con larghezza di 2 caratteri
+            numero_pari += 2; // Incremento al prossimo numero pari
+        }
+        printf("\n"); // Vado a capo dopo ogni riga
+    }
 }
