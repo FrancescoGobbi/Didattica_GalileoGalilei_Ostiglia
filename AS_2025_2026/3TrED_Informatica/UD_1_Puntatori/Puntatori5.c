f9c60e0c4a1eef88a80618eeb3c3d4ded4e1254d
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-10-09 15:00:52 +0200

    Add file C

---
+++ AS_2025_2026/3TrED_Informatica/UD_1_Puntatori/Puntatori5.c
@@ -0,0 +1,16 @@
+/* Puntatori 4
+Con l'utilizzo dei puntatori eseguire e stampare della seguente figura:
+* * * * * 
+*       *
+*       *
+* * * * *   
+
+con A la dimensione del lato del quadrato.
+L'utente deve inserire il valore di A compreso tra 1 e 20.
+*/
+
+#include <stdio.h>
+
+int main() {
+    // TO DO...
+}
