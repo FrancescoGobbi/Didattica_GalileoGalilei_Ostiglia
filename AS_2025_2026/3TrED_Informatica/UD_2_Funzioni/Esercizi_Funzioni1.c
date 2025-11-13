fc0285e89bd5b73d6eccb9c06464d4be320d9114
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-11-13 13:21:17 +0100

    Add file C

--- AS_2025_2026/3TrED_Informatica/UD_2_Funzioni/Esercizi_Funzioni1.c
+++ AS_2025_2026/3TrED_Informatica/UD_2_Funzioni/Esercizi_Funzioni1.c
@@ -14,10 +14,15 @@ Se A = 5 e B = 6 --> return della funzione = 5.5000 (perché (5+6)/2)
 
 */
 
+
 #include <stdio.h>
 
 // Prototipo delle funzioni da creare
-// TO DO...
+// Funzione 1
+int sommatoria(int n);
+
+// Funzione 2
+float fmedia(int a, int b);
 
 int main(){
     int n;
@@ -30,17 +35,58 @@ int main(){
     scanf("%d", &n);
 
     // Chiamata funzione sommatoria()
-
+    somma = sommatoria(n);
+    printf("Sono tornato nella funzione main().\n");
     printf("La sommatoria dei primi %d numeri interi è: %d\n", n, somma);
+
+    // Chiamata funzione sommatoria()
+    somma = sommatoria(10);
+    printf("Sono tornato nella funzione main().\n");
+    printf("La sommatoria dei primi %d numeri interi è: %d\n", 10, somma);
+
+    // Chiamata funzione sommatoria()
+    somma = sommatoria(100);
+    printf("Sono tornato nella funzione main().\n");
+    printf("La sommatoria dei primi %d numeri interi è: %d\n", 100, somma);
 
     // Funzione media()
     printf("Inserisci due numeri interi A e B per calcolare la loro media: ");
     scanf("%d %d", &A, &B);
 
-    // Chiamata funzione media()
-
+    // Chiamata funzione fmedia()
+    media = fmedia(A, B);
+    printf("Sono tornato nella funzione main().\n");
     printf("La media tra %d e %d è: %.4f\n", A, B, media);
+
+    // Chiamata funzione fmedia()
+    media = fmedia(8, 10);
+    printf("Sono tornato nella funzione main().\n");
+    printf("La media tra %d e %d è: %.4f\n", 8, 10, media);
+
+    // Chiamata funzione fmedia()
+    media = fmedia(1, 6);
+    printf("Sono tornato nella funzione main().\n");
+    printf("La media tra %d e %d è: %.4f\n", 1, 6, media);
 }
 
 // Definizione delle funzioni da creare
-// TO DO...
+// Funzione 1
+int sommatoria(int n) {
+    printf("Sono nella funzione sommatoria(): n=%d\n", n);
+    int somma = 0;
+    int i = 1;
+
+    for(i = 1; i<=n; i++){
+        somma += i; // somma = somma + i
+    }
+    
+    // Restituisco il valore
+    return somma;
+}
+
+// Funzione 2
+float fmedia(int a, int b) {
+    printf("Sono nella funzione fmedia(): a=%d b=%d\n", a, b);
+    float media = (a+b) / 2.0;
+    return media;
+}
