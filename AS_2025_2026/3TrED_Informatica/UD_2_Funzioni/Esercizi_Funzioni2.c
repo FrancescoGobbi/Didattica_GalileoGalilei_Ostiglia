0ff89e235081c110d2104958e3a64622e0766f36
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-11-14 09:14:32 +0100

    Add file C

---
+++ AS_2025_2026/3TrED_Informatica/UD_2_Funzioni/Esercizi_Funzioni2.c
@@ -0,0 +1,33 @@
+/*
+Esercizio: CREA LE SEGUENTI FUNZIONI IN C (DICHIARAZIONE E DEFINIZIONE + CHIAMATA NEL MAIN)
+
+
+FUNZIONE 1:
+Funzione che prende in input un valore intero n e restituisce 1 se il numero è primo, 0 altrimenti
+
+ESEMPIO: n = 7 --> return della funzione = 1 (perché 7 è un numero primo)
+
+FUNZIONE 2:
+Funzione che prende in input un valore intero n e restituisce la sommatoria dei primi numeri primi tra 1 ed n compresi
+
+ESEMPIO: n = 10 --> return della funzione = 17 (perché 2 + 3 + 5 + 7 = 17)
+*/
+
+
+#include <stdio.h>
+
+// Prototipo delle funzioni da creare
+// TO DO...
+
+int main(){
+    int n;
+
+    printf("Inserisci un numero intero n: ");
+    scanf("%d", &n);
+    // Funzione numeroPrimo()
+
+    
+}
+
+// Definizione delle funzioni da creare
+// TO DO...
