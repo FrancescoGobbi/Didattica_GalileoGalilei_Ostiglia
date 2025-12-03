71a7527efa5e5d455de4cb6d8327c95e8783620c
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-12-03 09:57:46 +0100

    Add file C

---
+++ AS_2025_2026/3TrED_Informatica/UD_2_Funzioni/Esercizi_Funzioni6.c
@@ -0,0 +1,34 @@
+/*
+Esercizio 6: CREA LE SEGUENTI FUNZIONI IN C (DICHIARAZIONE E DEFINIZIONE + CHIAMATA NEL MAIN)
+
+
+FUNZIONE 1:
+Creare una funzione in C che prenda in input un numero intero n.
+La funzione deve calcolare e restituire la maedia dei fattoriali dei numeri da 1 a n.
+ESEMPIO: n = 4 --> return della funzione = 9.75 (perchè (1! + 2! + 3! + 4!) / 4 = 9.75)
+
+FUNZIONE 2:
+Creare una funzione in C che prenda in input un numero intero a ed un numero intero b.
+La funzione deve calcolare e restituire il numero(qunatitativo) di numeri primi tra il numero a ed il numero b (compresi).
+ESEMPIO: a = 5, b = 15 --> return della funzione = 4 (perchè i numeri primi tra 5 e 15 sono: 5, 7, 11, 13 --> totale 4 numeri primi)
+*/
+
+#include <stdio.h>
+
+// Definizione delle funzioni da creare
+
+// FUNZIONE 1
+
+// FUNZIONE 2
+
+int main(){
+
+    
+}
+
+// Definizione delle funzioni da creare
+
+
+// FUNZIONE 1
+
+// FUNZIONE 2
