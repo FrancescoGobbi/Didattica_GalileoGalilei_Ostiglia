7010dcd1309911b94d843d1eb1e193cb97a7de17
FrancescoGobbi <francesco.gobbi4@gmail.com>
2025-12-03 09:44:13 +0100

    Add file C

---
+++ AS_2025_2026/3TrED_Informatica/UD_2_Funzioni/EserciziStack_Funzioni2.c
@@ -0,0 +1,37 @@
+/*
+Esercizi da interpretare per la crezione dello stack di evoluzione di un programma in C.
+Esercizi da svolgere come da spigazione a lezione. Vedi materiale di teoria.
+*/
+
+#include <stdio.h>
+
+int funzione1(int n);
+
+int funzione2(int a, int b);
+
+int main() {
+    int n, a, b;
+
+    printf("Inserisci un numero intero n: ");
+    scanf("%d", &n);
+    int risultato1 = funzione1(n);
+    printf("Il risultato della funzione 1 è: %d\n", risultato1);
+
+    printf("Inserisci due numeri interi a e b: ");
+    scanf("%d %d", &a, &b);
+    int risultato2 = funzione2(a, b);
+    printf("Il risultato della funzione 2 è: %d\n", risultato2);
+
+}   
+
+int funzione1(int n) {
+    // Implementazione della funzione 1
+    int risultato = n * n;
+    return risultato;
+}
+
+int funzione2(int a, int b) {
+    // Implementazione della funzione 2
+    int risultato = a + b;
+    return risultato;
+}
