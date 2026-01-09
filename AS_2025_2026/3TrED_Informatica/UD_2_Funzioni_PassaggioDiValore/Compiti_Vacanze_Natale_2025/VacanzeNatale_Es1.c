ccab533ed06402fb0a6f99c58514f52d0b5e3c67
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-01-09 09:42:58 +0100

    Add file C

---
+++ AS_2025_2026/3TrED_Informatica/UD_2_Funzioni_PassaggioDiValore/Compiti_Vacanze_Natale_2025/VacanzeNatale_Es1.c
@@ -0,0 +1,32 @@
+/*
+Crea una funzione in C che riceve in ingresso un numero intero positivo e restituisce il numero di cifre
+che lo compongono.
+ESEMPIO: se il numero è 5089, la funzione restituisce 4.
+
+*/
+
+#include <stdio.h>
+
+int conta_cifre(int n);
+
+int main() {
+
+    int numero;
+    printf("Inserisci un numero intero positivo: ");
+    scanf("%d", &numero);
+
+    int cifre = conta_cifre(numero);
+    printf("Il numero %d ha %d cifre.\n", numero, cifre);
+}
+
+int conta_cifre(int n) {
+    int count = 0;
+    if (n == 0) {
+        return 1; // Lo zero ha una cifra
+    }
+    while (n != 0) {
+        n = n / 10; // Rimuovo l'ultima cifra
+        count++;    // Incremento il contatore delle cifre
+    }
+    return count;
+}
