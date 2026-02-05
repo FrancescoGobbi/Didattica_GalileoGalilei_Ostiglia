bfe29000f7a24bd6563968288d9e9d3b1920f33a
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-02-05 10:33:47 +0100

    Add file C

--- AS_2025_2026/3TrED_Informatica/UD_3_Funzioni_PassaggioDiRiferimento/Funzioni_PassaggioRiferimento1.c
+++ AS_2025_2026/3TrED_Informatica/UD_3_Funzioni_PassaggioDiRiferimento/Funzioni_PassaggioRiferimento1.c
@@ -1,29 +1,91 @@
 #include <stdio.h>
+#include <math.h>
+
+/*
+N.B. Per creare una funzione in C è necessario:
+- Creare il prototipo della funzione, prima del main()
+- Definire la funzione che si vuole costruire, sotto al main()
+- Chiamare la funzione stessa nel main() o in altr funzioni
+*/
 
 /**
- * @brief Funzione per lo scambio di due variaibli
- * Funzione con passaggio di riferimenti
+ * @brief Funzione per l'incremento di una variabile
  * 
- * @param *a 
- * @param *b 
+ * @param x 
+ * @return int 
  */
-void swap(int *a, int *b);
+int incremento1 (int x);
+
+/**
+ * @brief Funzione per l'incremento di una variabile. 
+ * N.B. Funzione void NON ha return
+ * 
+ * @param x 
+ */
+void incremento2 (int x);
+
+/**
+ * @brief Funzione per l'incremento di una variabile puntatore.
+ * Viene passato il punatore della variabile che si vuole incrementare
+ * N.B. Funzione void NON ha return
+ * 
+ * @param x 
+ */
+void incremento3 (int *x);
 
 int main() {
-    int a = 5;
-    int b = 3;
+    int x = 5;
 
-    printf("a = %d, b = %d\n", a, b);
+    printf("\n\nSono nella funzione main()");
+    printf("Il valore di x è: %d\n", x);
+    printf("L'indirizzo di memoria della variabile x nella funzione main() è: %p\n", &x);
 
-    swap(&a, &b); // Chiamata di funzione con passaggio di riferimento (due riferimenti di variabili)
+    // incremento1()
+    /*Chiamata della funzione incremento1()
+    Il valore di x verrà sovrascritto con il valore che la funzione incremento1()
+    le restituisce*/
+    x = incremento1(x);
+    printf("\n\nSono tornato nella funzione main()\nIl valore di x è: %d\n", x);
+    printf("L'indirizzo di memoria della variabile x nella funzione main() è: %p\n", &x);
 
-    printf("a = %d, b = %d\n", a, b);
+    // incremento2()
+    /*Chiamata della funzione incremento2()
+    Il valore di x nel main() NON viene cambiato*/
+    incremento2(x);
+    printf("\n\nSono tornato nella funzione main()\nIl valore di x è: %d\n", x);
+    printf("L'indirizzo di memoria della variabile x nella funzione main() è: %p\n", &x);
+
+    // incremento3()
+    /*Chiamata della funzione incremento3()
+    Il valore di x nel main() viene cambiato perché viene passato il 
+    riferimento di memoria della variabile x del main() e nella funzione incremento3()
+    viene utilizzato il puntatore per incrementare il valore.*/
+    incremento3(&x);
+    printf("\n\nSono tornato nella funzione main()\nIl valore di x è: %d\n", x);
+    printf("L'indirizzo di memoria della variabile x nella funzione main() è: %p\n", &x);
 }
 
-// Funzione Swap che va a modificare i valori in memoria nel main, perché passati per riferimento
-void swap(int *a, int *b) {
-    int tmp;
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+int incremento1 (int x) {
+    printf("\nSono dentro la funzione incremento1()\n");
+    printf("Il valore di x è: %d\n", x);
+    printf("L'indirizzo di memoria della variabile x nella funzione incremento1() è: %p\n", &x);
+    x++; // Incremento la variabile locale alla variabile
+    return x; // restutisco il valore della variabile locale x alla funzione chiamante
+}
+
+void incremento2 (int x) {
+    printf("\nSono dentro la funzione incremento2()\n");
+    printf("Il valore di x è: %d\n", x);
+    printf("L'indirizzo di memoria della variabile x nella funzione incremento2() è: %p\n", &x);
+    x++; // Incremento la variabile locale della funzione
+    // N.B. Funzione void NON ha return
+}
+
+void incremento3 (int *x) {
+    printf("\nSono dentro la funzione incremento3()\n");
+    printf("Il valore di x è: %p\n", x); // Stampo il contenuto della variabile x
+    // ovvero l'indirizzo di memoria della variabile x del main() che è contenuto all'interno
+    // della variaible x della funzione incremento3()
+    printf("L'indirizzo di memoria della variabile x nella funzione incremento2() è: %p\n", &x); // Indirizzo di memoria di x dell'incremento3()
+    (*x)++; // Incremento il valore PUNTATO dalla variabile x
 }
