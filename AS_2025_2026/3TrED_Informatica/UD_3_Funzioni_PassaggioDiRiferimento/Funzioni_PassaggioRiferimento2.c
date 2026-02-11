099124bb4e072e0ef19fce404fcf855c5f13b43a
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-02-11 17:41:40 +0100

    Add file C

--- AS_2025_2026/3TrED_Informatica/UD_3_Funzioni_PassaggioDiRiferimento/Funzioni_PassaggioRiferimento2.c
+++ AS_2025_2026/3TrED_Informatica/UD_3_Funzioni_PassaggioDiRiferimento/Funzioni_PassaggioRiferimento2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * @brief Funzione che prende in input il riferimento di tre variabili e calcola la media
  * 
@@ -9,6 +8,8 @@
  * @param *media 
  */
 void media(int *somma, int *count, float *media);
+
+void calcoloMedia(int *n, int *somma, float *media);
 
 int main() {
     int somma = 35;
@@ -20,9 +21,27 @@ int main() {
     printf("La somma è: %d\n", somma);
     printf("Il count è: %d\n", count);
     printf("La media è: %.2f\n", media_result);
+
+    int n = 4;
+    somma = 0; // Reset della somma per il nuovo calcolo
+    media_result = 0.0; // Reset della media per il nuovo calcolo
+    calcoloMedia(&n, &somma, &media_result);
+
+    printf("La somma dei numeri inseriti è: %d\n", somma);
+    printf("La media dei numeri inseriti è: %.2f\n", media_result);
 }
 
 void media(int *somma, int *count, float *media) {
     // Calcolo della media
     *media = (float)(*somma) / (*count);
 }  
+
+void calcoloMedia(int *n, int *somma, float *media) {
+    int i, num;
+    for (i = 1; i <= *n; i++) {
+        printf("Inserisci un numero: ");
+        scanf("%d", &num);
+        *somma += num; // Aggiorna la somma con il nuovo numero
+    }
+    *media = (float)(*somma) / (*n); // Calcola la media
+}
