04373caf99b033dcca7800c89cceb310063efa5d
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-03-01 23:00:42 +0100

    Add file Java

---
+++ AS_2025_2026/5I_TPSI/UD_3/Semplice.java
@@ -0,0 +1,7 @@
+import java.lang.*; // Importa la libreria standard di Java 
+    public class Semplice { // Definisce una classe pubblica chiamata Semplice, che è il punto di ingresso del programma
+        // Il nome della classe deve corrispondere al nome del file (Semplice.java)
+        public static void main(String[] args) { // Il metodo main è il punto di partenza dell'esecuzione del programma
+            System.out.println("Questo è un semplice programma Java."); // Stampa una stringa sulla console
+        }
+}
