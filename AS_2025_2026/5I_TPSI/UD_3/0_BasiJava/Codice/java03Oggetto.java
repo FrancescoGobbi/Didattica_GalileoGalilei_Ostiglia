ab6fb8f711c91c0caaeef8312aa5baa39eb63176
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-03-09 08:59:04 +0100

    Add file Java

--- AS_2025_2026/5I_TPSI/UD_3/0_BasiJava/Codice/java03Oggetto.java
+++ AS_2025_2026/5I_TPSI/UD_3/0_BasiJava/Codice/java03Oggetto.java
@@ -23,14 +23,19 @@ public class java03Oggetto {
         int lunghezzaStringa = stringa.length(); // Il metodo length() restituisce la lunghezza della stringa stringa
         System.out.println("La lunghezza della stringa è: " + lunghezzaStringa); // Stampa la lunghezza della stringa stringa
         // Il '+' nella stampa viene utilizzato per concatenare la stringa "La lunghezza della stringa è: " con il il valore dela variabile primitiva lunghezzaStringa, che è un intero rappresentante la lunghezza della stringa.
-    
+
+        // Utilizzo di un altro oggetto String
         String prova = "Oggi è il 9 Marzo 2026";
 
+        // Stampa del valore dell'oggetto denominato prova
         System.out.println(prova);
 
+        // Utilizzo di un metodo dell'oggetto String per convertire la stringa in maiuscolo
+        // Non viene modificato l'oggetto prova, ma viene creato un nuovo oggetto String con il risultato della conversione in maiuscolo
+        // Questo oggetto, però, non viene mai salvato e quindi non più accessibile!
         System.out.println(prova.toUpperCase());
 
+        // Salvo il risultato della conversione in maiuscolo in un oggetto di tipo String
         String prova2 = prova.toUpperCase();
-        
     }
 }
