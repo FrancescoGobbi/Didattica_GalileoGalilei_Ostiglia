205d7c210c5d0d7ae9f545009bd4961a5f54f7cf
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-03-20 14:00:57 +0100

    Add file Java

--- AS_2025_2026/5I_TPSI/UD_3/0_BasiJava/Codice/EserciziLaboratorio/EserciziSvolti/Esercizio01.java
+++ AS_2025_2026/5I_TPSI/UD_3/0_BasiJava/Codice/EserciziLaboratorio/EserciziSvolti/Esercizio01.java
@@ -1,5 +1,6 @@
 package EserciziSvolti;
 import java.lang.*; // Importa la libreria standard di Java
+import java.util.Scanner;
 
 
 public class Esercizio01 {
@@ -9,5 +10,25 @@ public class Esercizio01 {
         // e verifica che la somma tra i due numeri è un 
         // numero pari o dispari
         // TO DO...
+
+        // Creo l'oggetto Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Prendo in input i due valori interi
+        System.out.print("Prendo il primo numero intero");
+        int val1 = sc .nextInt();
+        System.out.print("Prendo il secondo numero intero");
+        int val2 = sc .nextInt();
+
+        // Creo la somma
+        int somma = val1 + val2;
+
+        // Verifico la somma se pari o dispari
+        if (somma % 2 == 0) {
+            System.out.print("La somma è pari");
+        } 
+        else{
+            System.out.print("La somma è dispari");
+        }
     }
 }
