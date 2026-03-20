2b3a2597a0079fffeb52e8bdffe8b9f8a6508d9f
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-03-20 13:54:33 +0100

    Add file

---
+++ AS_2025_2026/5I_TPSI/UD_3/0_BasiJava/Codice/EserciziLaboratorio/EserciziSvolti/Esercizio01.java
@@ -0,0 +1,13 @@
+package EserciziSvolti;
+import java.lang.*; // Importa la libreria standard di Java
+
+
+public class Esercizio01 {
+    public static void main(String[] args) {
+
+        // Creare un codice in Java che prende in input due numeri
+        // e verifica che la somma tra i due numeri è un 
+        // numero pari o dispari
+        // TO DO...
+    }
+}
---
+++ AS_2025_2026/5I_TPSI/UD_3/0_BasiJava/Codice/EserciziLaboratorio/EserciziSvolti/readme.md
@@ -0,0 +1,6 @@
+### *Dispense 5I Anno Scolastico 2025/2026 - Materia : TPSI (Tecnologie e Progettazione di Sistemi Informatici e di telecomunicazioni)*
+
+**Esercizi visti in teoria di TPSI ****
+
+**Argomenti Trattati negli Esercizi**
+- [X] Esercizio01.java : prendere in inptu due valori, sommarli e verificare se la somma è pari o dispari
