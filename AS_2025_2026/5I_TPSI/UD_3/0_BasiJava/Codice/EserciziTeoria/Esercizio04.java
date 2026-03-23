6fc3dd54e29a69d316f27480ade67a9ef0391627
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-03-23 09:26:01 +0100

    Add file Java

---
+++ AS_2025_2026/5I_TPSI/UD_3/0_BasiJava/Codice/EserciziTeoria/Esercizio04.java
@@ -0,0 +1,16 @@
+import java.lang.*; // Importa la libreria standard di Java
+
+public class Esercizio04 {
+    public static void main(String[] args) {
+
+        // Creare una classe associata a questo esercizio per la 
+        // creazione di oggetti Esercizio04_Triangolo
+        // che permetta la creazione di un oggetto Triangolo, 
+        // con gli attributi basilari ed i metodi basilari e necessari
+        // oltre a tutti i metodi necessari per calcolare l'area 
+        // e il perimetro di un triangolo
+
+        // Creare due oggetti Esercizio04_Triangolo e provare i vari metodi creati
+
+    }
+}
