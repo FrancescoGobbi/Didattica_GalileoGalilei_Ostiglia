87d04db564244dfe87b520e85e53e8f211076dec
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-03-27 11:30:43 +0100

    Add file Java

---
+++ AS_2025_2026/5I_TPSI/UD_3/0_BasiJava/Codice/EserciziTeoria/Esercizio05.java
@@ -0,0 +1,11 @@
+import java.lang.*; // Importa la libreria standard di Java
+
+public class Esercizio05 {
+    public static void main(String[] args) {
+
+        // Creare un codice in Java che prenda in input un numero 
+        //intero positivo (verificare che l'imput sia corretto) 
+        //e stampi tutti i numeri primi minori o uguali a quel numero
+
+    }
+}
---
+++ AS_2025_2026/5I_TPSI/UD_3/0_BasiJava/Codice/EserciziTeoria/Esercizio06.java
@@ -0,0 +1,13 @@
+import java.lang.*; // Importa la libreria standard di Java
+
+public class Esercizio06 {
+    public static void main(String[] args) {
+
+        // Creare una classe in Java che rappresenti un oggetto "Libro" 
+        // con i seguenti attributi: titolo, autore, anno di pubblicazione e numero di pagine.
+        // La classe deve avere un costruttore che inizializza tutti gli attributi 
+        // e deve avere un metodo che stampi tutte le informazioni del libro in modo leggibile.
+        // Creare due oggetti "Libro" e stampare le loro informazioni utilizzando il metodo creato.
+
+    }
+}
