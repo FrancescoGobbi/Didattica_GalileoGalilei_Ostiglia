289d4c32aa9cfde5e42f7a1bb39acec9e12e0905
FrancescoGobbi <francesco.gobbi4@gmail.com>
2026-05-07 12:23:08 +0200

    Add file

--- AS_2025_2026/3TrED_Informatica/UD_5_Array/Esercizi/Array_Esercizio01.c
+++ AS_2025_2026/3TrED_Informatica/UD_5_Array/Esercizi/Array_Esercizio01.c
@@ -12,5 +12,27 @@ Il codice deve trovare il valore minimo tra i valori dell'array e stamparlo a vi
 # define dim 10
 
 int main(){
-    // TO DO...
+    int array[dim];
+    int i, min;
+    srand(time(0));
+    int n, m;
+    do{
+        printf("Inserisci il valore minimo (n): ");
+        scanf("%d", &n);
+        printf("Inserisci il valore massimo (m): ");
+        scanf("%d", &m);
+    } while(n <= 0 || m <= 0 || n >= m);
+
+    for (i = 0; i < dim; i++) {
+        array[i] = rand() % (m - n + 1) + n;
+    }
+
+    min = array[0];
+    
+    for (i = 1; i < dim; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    printf("Il valore minimo è: %d\n", min);
 }
